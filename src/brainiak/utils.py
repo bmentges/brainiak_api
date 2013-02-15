0c4d7cc9f523c2cbfba9a9a6e4cc42cb4e8c548e
rodsenra <rodsenra@gmail.com>
2013-02-15 18:31:52 -0200

    Extracted result handler from api_semantica and adapted it to brainiak. Coverage not complete yet

--- src/brainiak/utils.py
+++ src/brainiak/utils.py
@@ -1,5 +1,6 @@
 from tornado.httpclient import AsyncHTTPClient
 
+
 def get_async_client(self):
     AsyncHTTPClient.configure("tornado.curl_httpclient.CurlAsyncHTTPClient")
     return AsyncHTTPClient()

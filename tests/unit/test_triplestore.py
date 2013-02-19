e6a1eab6a5236ecfcfbfa6a610283ab53d50aed1
icaromedeiros <icaro.medeiros@gmail.com>
2013-02-19 11:43:57 -0300

    triplestore with a async tornado client. beware: query methods not tested, only constructor

--- tests/unit/test_triplestore.py
+++ tests/unit/test_triplestore.py
@@ -1,14 +1,25 @@
 # coding: utf-8
-
-import unittest
 from mock import patch
+
+import tornado
+from tornado.testing import AsyncTestCase
+from tornado.ioloop import IOLoop
+
 from brainiak import triplestore
 
 
-class TriplestoreWithFullUrlTestCase(unittest.TestCase):
+class TriplestoreTestCase(AsyncTestCase):
+
+    def setUp(self):
+        self.io_loop = self.get_new_ioloop()
+
+    def tearDown(self):
+        if (not IOLoop.initialized() or self.io_loop is not IOLoop.instance()):
+            self.io_loop.close(all_fds=True)
+        super(AsyncTestCase, self).tearDown()
 
     @patch('brainiak.triplestore.settings', SPARQL_ENDPOINT_HOST="http://myhost", SPARQL_ENDPOINT_PORT=8080)
-    def test_endpoint_host_and_port_defined_in_settings(self, settings):
+    def test_init_connection_endpoint_host_and_port_defined_in_settings(self, settings):
         del settings.SPARQL_ENDPOINT_FULL_URL
 
         expected = "http://myhost:8080/sparql"
@@ -17,8 +28,17 @@ class TriplestoreWithFullUrlTestCase(unittest.TestCase):
         self.assertEquals(expected, result)
 
     @patch('brainiak.triplestore.settings', SPARQL_ENDPOINT_FULL_URL="http://myhost:8080/sparql")
-    def test_endpoint_full_url_defined_in_settings(self, settings):
+    def test_init_connection_endpoint_full_url_defined_in_settings(self, settings):
         expected = "http://myhost:8080/sparql"
         virtuoso_connection = triplestore.VirtuosoConnection()
         result = virtuoso_connection.endpoint_url
         self.assertEquals(expected, result)
+
+    @patch('tornado.ioloop.IOLoop', am_i_a_mock=True)
+    def test_init_connection_io_loop_as_a_param(self, io_loop):
+        virtuoso_connection = triplestore.VirtuosoConnection(io_loop)
+        self.assertTrue(virtuoso_connection.io_loop.am_i_a_mock)
+
+    def test_init_connection_io_loop_default(self):
+        virtuoso_connection = triplestore.VirtuosoConnection()
+        self.assertTrue(isinstance(virtuoso_connection.io_loop, tornado.ioloop.IOLoop))

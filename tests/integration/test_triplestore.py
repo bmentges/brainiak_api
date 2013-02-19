062b07492da4c644a3c68193522bb1113b8b5b00
icaromedeiros <icaro.medeiros@gmail.com>
2013-02-19 15:48:53 -0300

    test case utility for tornado in tests/__init__.py for setup/teardown using ioloop and AsyncTestCase.wait without timeout

--- tests/integration/test_triplestore.py
+++ tests/integration/test_triplestore.py
@@ -1,18 +1,8 @@
-from tornado.testing import AsyncTestCase
-from tornado.ioloop import IOLoop
-
+from tests import TornadoAsyncTestCase
 from brainiak import triplestore
 
 
-class TriplestoreTestCase(AsyncTestCase):
-
-    def setUp(self):
-        self.io_loop = self.get_new_ioloop()
-
-    def tearDown(self):
-        if (not IOLoop.initialized() or self.io_loop is not IOLoop.instance()):
-            self.io_loop.close(all_fds=True)
-        super(AsyncTestCase, self).tearDown()
+class TriplestoreTestCase(TornadoAsyncTestCase):
 
     def is_response_ok(self, response):
         self.assertEquals(response.code, 200)
@@ -22,4 +12,4 @@ class TriplestoreTestCase(AsyncTestCase):
         SIMPLE_COUNT_CLASSES_QUERY = "SELECT COUNT(*) WHERE {?s a owl:Class}"
         virtuoso_connection = triplestore.VirtuosoConnection(self.io_loop)
         virtuoso_connection.query(self.is_response_ok, SIMPLE_COUNT_CLASSES_QUERY)
-        self.wait(timeout=None)  # disable timeout for debugging purposes
+        self.wait()

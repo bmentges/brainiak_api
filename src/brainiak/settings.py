39c910fed74a45bb5caf7f10ebf46d510f8eb706
icaromedeiros <icaro.medeiros@gmail.com>
2013-02-20 17:55:38 -0300

    authenticated access to virtuoso using patched-tornado

--- src/brainiak/settings.py
+++ src/brainiak/settings.py
@@ -10,6 +10,7 @@ SPARQL_ENDPOINT_PORT = 8890
 
 SPARQL_ENDPOINT_USER = "api-semantica"
 SPARQL_ENDPOINT_PASSWORD = "api-semantica"
+SPARQL_ENDPOINT_AUTH_MODE = "api-semantica"
 SPARQL_ENDPOINT_REALM = "SPARQL"  # Virtuoso-related
 
 URI_PREFIX = "http://semantica.globo.com"
--- tests/integration/test_triplestore.py
+++ tests/integration/test_triplestore.py
@@ -1,7 +1,6 @@
 from tests import TornadoAsyncTestCase
 from brainiak import triplestore
 
-
 class TriplestoreTestCase(TornadoAsyncTestCase):
 
     def is_response_ok(self, response, *args, **kw):
@@ -23,6 +22,12 @@ class TriplestoreTestCase(TornadoAsyncTestCase):
         virtuoso_connection = triplestore.VirtuosoConnection(self.io_loop)
         virtuoso_connection.query(self.is_response_ok, SIMPLE_COUNT_CLASSES_QUERY, method="GET")
         self.wait()
+
+    def test_query_ok_with_unsupported_method(self):
+        SIMPLE_COUNT_CLASSES_QUERY = "SELECT COUNT(*) WHERE {?s a owl:Class}"
+        virtuoso_connection = triplestore.VirtuosoConnection(self.io_loop)
+        virtuoso_connection.query(self.raises_virtuoso_exception, SIMPLE_COUNT_CLASSES_QUERY, method="UNSUPPORTED")
+        self.wait(timeout=1)
 
     def test_malformed_query(self):
         SIMPLE_COUNT_CLASSES_QUERY = "SELECT A MALFORMED QUERY WHERE {?s a owl:Class}"

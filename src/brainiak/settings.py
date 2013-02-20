a9c19b979da774da4cee6cb2b927b0e886199868
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2013-02-20 11:29:32 -0300

    Add sparql endpoint password and user, for digest auth

--- src/brainiak/settings.py
+++ src/brainiak/settings.py
@@ -7,5 +7,7 @@ SERVER_PORT = 5100
 SPARQL_ENDPOINT_DATABASE = "virtuoso"
 SPARQL_ENDPOINT_HOST = "http://localhost"
 SPARQL_ENDPOINT_PORT = 8890
+SPARQL_ENDPOINT_PASSWORD = "api-semantica"
+SPARQL_ENDPOINT_USER = "api-semantica"
 
 URI_PREFIX = "http://test.domain.com"

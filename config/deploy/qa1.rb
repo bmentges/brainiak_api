5e9096fcfa90841dd1d17ae748c98b9e0c36290c
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2013-02-21 19:03:19 -0300

    Deploy based on existing api_semantica (v1)

---
+++ config/deploy/qa1.rb
@@ -0,0 +1,12 @@
+# Servers
+role :app, "cittavld45.globoi.com"  # deploys app source to filer
+role :restart, "cittavld45.globoi.com", :no_release => true # application
+role :docs, "cittavld45.globoi.com", :no_release => true  
+
+set :sparql_endpoint, "http://qa1.virtuoso.globoi.com:8890/sparql-auth"
+
+# Lazy...
+set :password, 'busca'
+
+# Used by puppet module in Capistrano
+set :puppetmaster_env, "qa1"

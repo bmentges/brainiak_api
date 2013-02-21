7bc0b720392688ae7b18b23833aad930951a20c9
icaromedeiros <icaro.medeiros@gmail.com>
2013-02-21 19:01:06 -0300

    uncommenting tests

--- tests/integration/test_triplestore.py
+++ tests/integration/test_triplestore.py
@@ -11,30 +11,30 @@ class TriplestoreTestCase(TornadoAsyncTestCase):
     def is_response_ok(self, response, *args, **kw):
         self.assertEquals(response.code, 200)
         self.stop()
-#
-#    def is_malformed_query(self, response, *args, **kw):
-#        self.assertEquals(response.code, 400)
-#        self.stop()
-#
-#    def is_unauthorized(self, response, *args, **kw):
-#        self.assertEquals(response.code, 401)
-#        self.stop()
-#
-#    def test_query_ok(self):
-#        virtuoso_connection = triplestore.VirtuosoConnection(self.io_loop)
-#        virtuoso_connection.query(self.is_response_ok, SIMPLE_COUNT_CLASSES_QUERY)
-#        self.wait()
-#
-#    def test_query_ok_with_get_method(self):
-#        virtuoso_connection = triplestore.VirtuosoConnection(self.io_loop)
-#        virtuoso_connection.query(self.is_response_ok, SIMPLE_COUNT_CLASSES_QUERY, method="GET")
-#        self.wait()
-#
-#    def test_malformed_query(self):
-#        MALFORMED_QUERY = "SELECT A MALFORMED QUERY {?s ?p ?o}"
-#        virtuoso_connection = triplestore.VirtuosoConnection(self.io_loop)
-#        virtuoso_connection.query(self.is_malformed_query, MALFORMED_QUERY)
-#        self.wait()
+
+    def is_malformed_query(self, response, *args, **kw):
+        self.assertEquals(response.code, 400)
+        self.stop()
+
+    def is_unauthorized(self, response, *args, **kw):
+        self.assertEquals(response.code, 401)
+        self.stop()
+
+    def test_query_ok(self):
+        virtuoso_connection = triplestore.VirtuosoConnection(self.io_loop)
+        virtuoso_connection.query(self.is_response_ok, SIMPLE_COUNT_CLASSES_QUERY)
+        self.wait()
+
+    def test_query_ok_with_get_method(self):
+        virtuoso_connection = triplestore.VirtuosoConnection(self.io_loop)
+        virtuoso_connection.query(self.is_response_ok, SIMPLE_COUNT_CLASSES_QUERY, method="GET")
+        self.wait()
+
+    def test_malformed_query(self):
+        MALFORMED_QUERY = "SELECT A MALFORMED QUERY {?s ?p ?o}"
+        virtuoso_connection = triplestore.VirtuosoConnection(self.io_loop)
+        virtuoso_connection.query(self.is_malformed_query, MALFORMED_QUERY)
+        self.wait()
 
     # HAPPY paths
     @patch("brainiak.triplestore.settings",
@@ -51,15 +51,15 @@ class TriplestoreTestCase(TornadoAsyncTestCase):
     def test_not_authenticated_acess_to_not_authenticated_endpoint(self):
         pass  # test_query_ok (above)
 
-#    # UNHAPPY paths
-#    @patch("brainiak.triplestore.settings", SPARQL_ENDPOINT_FULL_URL="http://localhost:8890/sparql-auth")
-#    def test_not_authenticated_access_to_authenticated_endpoint(self, settings):
-#        del settings.SPARQL_ENDPOINT_AUTH_MODE
-#        del settings.SPARQL_ENDPOINT_USER
-#        del settings.SPARQL_ENDPOINT_PASSWORD
-#        virtuoso_connection = triplestore.VirtuosoConnection(self.io_loop)
-#        virtuoso_connection.query(self.is_unauthorized, SIMPLE_COUNT_CLASSES_QUERY)
-#        self.wait()
-#
-#    def test_authenticated_access_to_not_authenticated_endpoint(self):
-#        self.fail("not implemented")
+    # UNHAPPY paths
+    @patch("brainiak.triplestore.settings", SPARQL_ENDPOINT_FULL_URL="http://localhost:8890/sparql-auth")
+    def test_not_authenticated_access_to_authenticated_endpoint(self, settings):
+        del settings.SPARQL_ENDPOINT_AUTH_MODE
+        del settings.SPARQL_ENDPOINT_USER
+        del settings.SPARQL_ENDPOINT_PASSWORD
+        virtuoso_connection = triplestore.VirtuosoConnection(self.io_loop)
+        virtuoso_connection.query(self.is_unauthorized, SIMPLE_COUNT_CLASSES_QUERY)
+        self.wait()
+
+    def test_authenticated_access_to_not_authenticated_endpoint(self):
+        self.fail("not implemented")

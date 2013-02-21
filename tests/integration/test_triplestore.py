94c2152c68f76869989c11546d4cf7b7d6b6de7f
icaromedeiros <icaro.medeiros@gmail.com>
2013-02-21 19:21:47 -0300

    merged my async with authentication with master and minor fixes

--- tests/integration/test_triplestore.py
+++ tests/integration/test_triplestore.py
@@ -20,29 +20,38 @@ class TriplestoreTestCase(TornadoAsyncTestCase):
         self.assertEquals(response.code, 401)
         self.stop()
 
-    def test_query_ok(self):
+    @patch("brainiak.triplestore.settings",
+           SPARQL_ENDPOINT="http://localhost:8890/sparql",
+           )
+    def test_query_ok(self, settings):
         virtuoso_connection = triplestore.VirtuosoConnection(self.io_loop)
         virtuoso_connection.query(self.is_response_ok, SIMPLE_COUNT_CLASSES_QUERY)
         self.wait()
 
-    def test_query_ok_with_get_method(self):
+    @patch("brainiak.triplestore.settings",
+           SPARQL_ENDPOINT="http://localhost:8890/sparql",
+           )
+    def test_query_ok_with_get_method(self, settings):
         virtuoso_connection = triplestore.VirtuosoConnection(self.io_loop)
         virtuoso_connection.query(self.is_response_ok, SIMPLE_COUNT_CLASSES_QUERY, method="GET")
         self.wait()
 
-    def test_malformed_query(self):
+    @patch("brainiak.triplestore.settings",
+           SPARQL_ENDPOINT="http://localhost:8890/sparql",
+           )
+    def test_malformed_query(self, settings):
         MALFORMED_QUERY = "SELECT A MALFORMED QUERY {?s ?p ?o}"
         virtuoso_connection = triplestore.VirtuosoConnection(self.io_loop)
         virtuoso_connection.query(self.is_malformed_query, MALFORMED_QUERY)
         self.wait()
 
-    # HAPPY paths
+    # Authentication HAPPY paths
     @patch("brainiak.triplestore.settings",
-           SPARQL_ENDPOINT_FULL_URL="http://localhost:8890/sparql-auth",
+           SPARQL_ENDPOINT="http://localhost:8890/sparql-auth",
            SPARQL_ENDPOINT_AUTH_MODE="digest",
            SPARQL_ENDPOINT_USER="api-semantica",
            SPARQL_ENDPOINT_PASSWORD="api-semantica"
-          )
+           )
     def test_authenticated_access_to_authenticated_endpoint(self, settings):
         virtuoso_connection = triplestore.VirtuosoConnection(self.io_loop)
         virtuoso_connection.query(self.is_response_ok, SIMPLE_COUNT_CLASSES_QUERY)
@@ -51,8 +60,8 @@ class TriplestoreTestCase(TornadoAsyncTestCase):
     def test_not_authenticated_acess_to_not_authenticated_endpoint(self):
         pass  # test_query_ok (above)
 
-    # UNHAPPY paths
-    @patch("brainiak.triplestore.settings", SPARQL_ENDPOINT_FULL_URL="http://localhost:8890/sparql-auth")
+    # Authentication UNHAPPY paths
+    @patch("brainiak.triplestore.settings", SPARQL_ENDPOINT="http://localhost:8890/sparql-auth")
     def test_not_authenticated_access_to_authenticated_endpoint(self, settings):
         del settings.SPARQL_ENDPOINT_AUTH_MODE
         del settings.SPARQL_ENDPOINT_USER

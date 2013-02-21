1054c22a3e2e9635bcef0345543365c90a4faea1
rodsenra <rodsenra@gmail.com>
2013-02-21 15:02:19 -0300

    Fix broken test case after handling contexts

--- src/brainiak/utils.py
+++ src/brainiak/utils.py
@@ -4,3 +4,4 @@ from tornado.httpclient import AsyncHTTPClient
 def get_tornado_async_client(io_loop=None):
     AsyncHTTPClient.configure("tornado.curl_httpclient.CurlAsyncHTTPClient")
     return AsyncHTTPClient(io_loop=io_loop)
+

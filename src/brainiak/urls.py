fd9f36b988436a0e5aa1eb7b08a5614a016b6ea0
rodsenra <rodsenra@gmail.com>
2013-02-22 10:45:01 -0300

    Changes in json-schema response do suit CMA demands

--- src/brainiak/urls.py
+++ src/brainiak/urls.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-from brainiak.resources import SchemaResource
+from brainiak.resources import SchemaResource, InstanceResource
 
 resources = [
-    (r'/(?P<context_name>.+)/schemas/(?P<schema_name>.+)', SchemaResource)
+    (r'/(?P<context_name>.+)/schemas/(?P<schema_name>.+)', SchemaResource),
+    (r'/(?P<context_name>.+)/collection/(?P<schema_name>.+)', InstanceResource)
 ]

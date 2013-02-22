12d7ddce2cfc1e802d556abf395d4eb1d801e289
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2013-02-22 10:12:42 -0300

    Add healthcheck and version resources

--- src/brainiak/urls.py
+++ src/brainiak/urls.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
-from brainiak.resources import SchemaResource
+from brainiak.resources import HealthcheckResource, SchemaResource, VersionResource
+
 
 resources = [
-    (r'/(?P<context_name>.+)/schemas/(?P<schema_name>.+)', SchemaResource)
+    (r'/(?P<context_name>.+)/schemas/(?P<schema_name>.+)', SchemaResource),
+    (r'/healthcheck', HealthcheckResource),
+    (r'/version', VersionResource)
 ]

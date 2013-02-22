1f57dbabbe244d34612af5aaf5aa3865f6489dbf
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2013-02-22 11:40:14 -0300

    Manual merge

--- src/brainiak/urls.py
+++ src/brainiak/urls.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
-from brainiak.resources import HealthcheckResource, SchemaResource, VersionResource, VirtuosoStatusResource
-
+from brainiak.resources import SchemaResource, InstanceResource, VersionResource, HealthcheckResource, VirtuosoStatusResource
 
 resources = [
+    (r'/(?P<context_name>.+)/schemas/(?P<schema_name>.+)', SchemaResource),
+    (r'/(?P<context_name>.+)/collection/(?P<schema_name>.+)', InstanceResource),
+
     (r'/healthcheck', HealthcheckResource),
     (r'/version', VersionResource),
     (r'/status/virtuoso', VirtuosoStatusResource),

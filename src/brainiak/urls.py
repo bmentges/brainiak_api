d5061c0f55dafbb76983fc759fe6eaca074309b1
rodsenra <rodsenra@gmail.com>
2013-02-26 17:29:07 -0300

    Changing /<ctx>/schemas/<schema_name> -> /<ctx>/<class_name>/_schema

--- src/brainiak/urls.py
+++ src/brainiak/urls.py
@@ -2,10 +2,9 @@
 from brainiak.resources import SchemaResource, VersionResource, HealthcheckResource, VirtuosoStatusResource  # InstanceResource
 
 resources = [
-    (r'/(?P<context_name>.+)/schemas/(?P<schema_name>.+)', SchemaResource),
+    (r'/(?P<context_name>.+)/(?P<class_name>.+)/_schema', SchemaResource),
 #    (r'/(?P<context_name>.+)/collection/(?P<schema_name>.+)', InstanceResource),
     (r'/healthcheck', HealthcheckResource),
     (r'/version', VersionResource),
-    (r'/status/virtuoso', VirtuosoStatusResource),
-    (r'/(?P<context_name>.+)/schemas/(?P<schema_name>.+)', SchemaResource)
+    (r'/status/virtuoso', VirtuosoStatusResource)
 ]

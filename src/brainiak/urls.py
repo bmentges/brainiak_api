d5fc2271f0cb6981f15f096047248f1b94a7b9a3
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2013-02-28 13:48:50 -0300

    Commented instance stuff, so we have a 100% covered code

--- src/brainiak/urls.py
+++ src/brainiak/urls.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 from brainiak.handlers import SchemaResource, VersionResource, \
-    HealthcheckResource, VirtuosoStatusResource, InstanceResource
+    HealthcheckResource, VirtuosoStatusResource  # , InstanceResource
 
 resources = [
     (r'/(?P<context_name>.+)/(?P<class_name>.+)/_schema', SchemaResource),
-    (r'/(?P<context_name>.+)/(?P<class_name>.+)/(?P<instance_id>.+)', InstanceResource),
+    #(r'/(?P<context_name>.+)/(?P<class_name>.+)/(?P<instance_id>.+)', InstanceResource),
     (r'/healthcheck', HealthcheckResource),
     (r'/version', VersionResource),
     (r'/status/virtuoso', VirtuosoStatusResource)

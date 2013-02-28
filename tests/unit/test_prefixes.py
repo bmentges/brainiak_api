d69a69aa0fc3b2a5844bd6f9f21adb8902f60b87
rodsenra <rodsenra@gmail.com>
2013-02-28 17:33:26 -0300

    Adds prefix_from_uri()

--- tests/unit/test_prefixes.py
+++ tests/unit/test_prefixes.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import unittest
-from brainiak.prefixes import prefix_to_slug, slug_to_prefix, shorten_uri, MemorizeContext, uri_to_slug
+from brainiak.prefixes import prefix_to_slug, slug_to_prefix, shorten_uri, MemorizeContext, uri_to_slug, prefix_from_uri
 
 
 class PrefixesTestCase(unittest.TestCase):
@@ -21,22 +21,25 @@ class PrefixesTestCase(unittest.TestCase):
     def test_shorten_uri_fails(self):
         self.assertEquals(shorten_uri("http://some/invalid/uri"), "http://some/invalid/uri")
 
+    def test_prefix_from_uri(self):
+        self.assertEquals("http://test/person/", prefix_from_uri("http://test/person/Person"))
+
 
 class MemorizeContextTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.remember = MemorizeContext()
+        self.context = MemorizeContext()
 
     def test_unique_object_properties(self):
-        self.remember.add_object_property("ctx:field_name", "upper:Entity")
-        self.assertIn('ctx:field_name', self.remember.object_properties)
+        self.context.add_object_property("ctx:field_name", "upper:Entity")
+        self.assertIn('ctx:field_name', self.context.object_properties)
 
     def test_prefix_to_slug(self):
-        self.assertEquals(self.remember.prefix_to_slug('http://www.w3.org/1999/02/22-rdf-syntax-ns#'), 'rdf')
-        self.assertIn('rdf', self.remember.context)
-        self.assertEquals(self.remember.context['rdf'], 'http://www.w3.org/1999/02/22-rdf-syntax-ns#')
+        self.assertEquals(self.context.prefix_to_slug('http://www.w3.org/1999/02/22-rdf-syntax-ns#'), 'rdf')
+        self.assertIn('rdf', self.context.context)
+        self.assertEquals(self.context.context['rdf'], 'http://www.w3.org/1999/02/22-rdf-syntax-ns#')
 
     def test_shorten_uri(self):
-        self.assertEquals(self.remember.shorten_uri('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'), 'rdf:type')
-        self.assertIn('rdf', self.remember.context)
-        self.assertEquals(self.remember.context['rdf'], 'http://www.w3.org/1999/02/22-rdf-syntax-ns#')
+        self.assertEquals(self.context.shorten_uri('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'), 'rdf:type')
+        self.assertIn('rdf', self.context.context)
+        self.assertEquals(self.context.context['rdf'], 'http://www.w3.org/1999/02/22-rdf-syntax-ns#')

f8220fec20b0faf7d0486a66b04de4d55455b20b
icaromedeiros <icaro.medeiros@gmail.com>
2013-03-07 11:03:19 -0300

    forgot to add :(

---
+++ src/brainiak/settings_ci.py
@@ -0,0 +1,18 @@
+import logging
+
+DEBUG = True
+ENVIRONMENT = "local"
+
+SERVER_PORT = 5100
+
+SPARQL_ENDPOINT = "http://localhost:8890/sparql-auth"
+SPARQL_ENDPOINT_USER = "api-semantica"
+SPARQL_ENDPOINT_PASSWORD = "api-semantica"
+SPARQL_ENDPOINT_AUTH_MODE = "digest"
+SPARQL_ENDPOINT_REALM = "SPARQL"
+
+LOG_FILEPATH = '/tmp/brainiak.log'
+LOG_LEVEL = logging.DEBUG
+LOG_NAME = "brainiak"
+
+URI_PREFIX = "http://semantica.globo.com"

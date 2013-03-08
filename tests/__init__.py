fee7f988acfebc15205c3d9c9ae0bc48bf46f679
rodsenra <rodsenra@gmail.com>
2013-03-08 18:31:38 -0300

    Migrating tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,28 +6,12 @@ from brainiak import server
 
 class TornadoAsyncTestCase(AsyncTestCase):
 
-    def setUp(self):
-        self.io_loop = self.get_new_ioloop()
-
-    def tearDown(self):
-        if (not IOLoop.initialized() or self.io_loop is not IOLoop.instance()):
-            self.io_loop.close(all_fds=True)
-        super(AsyncTestCase, self).tearDown()
-
     # Disabling timeout for debugging purposes
     def wait(self, condition=None, timeout=None):
         return super(TornadoAsyncTestCase, self).wait(condition, timeout)
 
 
 class TornadoAsyncHTTPTestCase(AsyncHTTPTestCase):
-
-    def setUp(self):
-        self.io_loop = self.get_new_ioloop()
-
-    def tearDown(self):
-        if (not IOLoop.initialized() or self.io_loop is not IOLoop.instance()):
-            self.io_loop.close(all_fds=True)
-        super(AsyncHTTPTestCase, self).tearDown()
 
     # Disabling timeout for debugging purposes
     def wait(self, condition=None, timeout=None):

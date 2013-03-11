6ffe9d2a71003d02418733ba80ac446b15f096e8
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2013-03-11 16:51:19 -0300

    Fix: return 404 when instance doesn't exist & Improvement: settings.URI_PREFIX ends with /, in order to keep up to definition on prefixes.py

--- tests/integration/test_resources.py
+++ tests/integration/test_resources.py
@@ -45,7 +45,7 @@ class TestInstanceResource(TestHandlerBase):
     def test_get_instance_with_nonexistent_uri(self):
         self.http_client.fetch(self.get_url('/person/Gender/Alien'), self.stop)
         response = self.wait()
-        self.assertEqual(response.code, 204)
+        self.assertEqual(response.code, 404)
 
     def test_get_instance(self):
         self.http_client.fetch(self.get_url('/person/Gender/Male'), self.stop)

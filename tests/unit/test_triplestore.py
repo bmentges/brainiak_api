2a296f2c6f5d6008e69a07f982d20bf7fb73e810
rodsenra <rodsenra@gmail.com>
2013-03-11 11:32:29 -0300

    Testing

--- tests/unit/test_triplestore.py
+++ tests/unit/test_triplestore.py
@@ -5,8 +5,7 @@ import SPARQLWrapper
 import tornado
 from mock import patch
 
-#from brainiak import triplestore, greenlet_tornado
-from brainiak import triplestore
+from brainiak import triplestore, greenlet_tornado
 from tests import TornadoAsyncTestCase
 
 
@@ -28,17 +27,17 @@ class TriplestoreInitTestCase(TornadoAsyncTestCase):
         result = virtuoso_connection.endpoint_url
         self.assertEquals(expected, result)
 
-    # @patch('tornado.ioloop.IOLoop', am_i_a_mock=True)
-    # @patch('brainiak.triplestore.settings', SPARQL_ENDPOINT="http://myhost:8080/sparql")
-    # def test_init_connection_io_loop_as_a_param(self, io_loop, settings):
-    #     virtuoso_connection = triplestore.VirtuosoConnection(io_loop)
-    #     self.assertTrue(greenlet_tornado._io_loop.am_i_a_mock)
-    #
-    # @patch('brainiak.triplestore.settings', SPARQL_ENDPOINT="http://myhost:8080/sparql")
-    # def test_init_connection_io_loop_default(self, settings):
-    #     virtuoso_connection = triplestore.VirtuosoConnection()
-    #     self.assertTrue(isinstance(greenlet_tornado._io_loop, tornado.ioloop.IOLoop))
-    #
+    @patch('tornado.ioloop.IOLoop', am_i_a_mock=True)
+    @patch('brainiak.triplestore.settings', SPARQL_ENDPOINT="http://myhost:8080/sparql")
+    def test_init_connection_io_loop_as_a_param(self, io_loop, settings):
+        virtuoso_connection = triplestore.VirtuosoConnection(io_loop)
+        self.assertTrue(greenlet_tornado._io_loop.am_i_a_mock)
+
+    @patch('brainiak.triplestore.settings', SPARQL_ENDPOINT="http://myhost:8080/sparql")
+    def test_init_connection_io_loop_default(self, settings):
+        virtuoso_connection = triplestore.VirtuosoConnection()
+        self.assertTrue(isinstance(greenlet_tornado._io_loop, tornado.ioloop.IOLoop))
+
 
 class TriplestoreSetCredentialsTestCase(TornadoAsyncTestCase):
 

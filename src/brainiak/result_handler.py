32bba3bbf77c4df7ac7d88e15be1070bad861b21
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2013-03-14 15:12:58 -0300

    List instances: Changed subject,label -> @id, title

--- src/brainiak/result_handler.py
+++ src/brainiak/result_handler.py
@@ -14,12 +14,12 @@ def filter_values(result_dict, key):
     return [item[key]['value'] for item in result_dict['results']['bindings'] if item.get(key)]
 
 
-def compress_keys_and_values(result_dict):
+def compress_keys_and_values(result_dict, keymap={"label": "title", "subject": "@id"}):
     result_list = []
     for item in result_dict['results']['bindings']:
         row = {}
         for key in item:
-            row[key] = item[key]['value']
+            row[keymap.get(key, key)] = item[key]['value']
         result_list.append(row)
     return result_list
 

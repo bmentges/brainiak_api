775a1d2dc6717e7b7696a0cab587b0efe90054ea
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2013-03-19 18:36:42 -0300

    Refactor links to utils/links.py

---
+++ src/brainiak/utils/links.py
@@ -0,0 +1,98 @@
+from math import ceil
+from urllib import urlencode
+from urlparse import parse_qs
+
+
+# TODO: UNIT TEST
+def set_query_string_parameter(query_string, param_name, param_value):
+    query_params = parse_qs(query_string)
+    query_params[param_name] = [param_value]
+    new_query_string = urlencode(query_params, doseq=True)
+    return new_query_string
+
+
+# TODO: UNIT TEST
+def get_last_page(total_items, per_page):
+    return int(ceil(total_items / float(per_page)))
+
+
+# TODO: UNIT TEST
+def get_previous_page(page):
+    if page > 1:
+        return page - 1
+    else:
+        return False
+
+
+# TODO: UNIT TEST
+def get_next_page(page, last_page):
+    if page < last_page:
+        return page + 1
+    else:
+        return False
+
+
+def build_links(class_uri, page, per_page, request_uri, total_items, query_string):
+
+    last_page = get_last_page(total_items, per_page)
+
+    links = [
+        {
+            'href': request_uri,
+            'rel': "self"
+        },
+        {
+            'href': class_uri,
+            'rel': "list"
+        },
+        {
+            'href': "%s/{resource_id}" % class_uri,
+            'rel': "item"
+        },
+        {
+            'href': class_uri,
+            'method': "POST",
+            'rel': "create"
+        },
+        {
+            'href': "%s/{resource_id}" % class_uri,
+            'method': "DELETE",
+            'rel': "delete"
+
+        },
+        {
+            'href': "%s/{resource_id}" % class_uri,
+            'method': "PATCH",
+            'rel': "edit"
+
+        },
+        {
+            'href': "%s?%s" % (class_uri, set_query_string_parameter(query_string, "page", "1")),
+            'method': "GET",
+            'rel': "first"
+        },
+        {
+            'href': "%s?%s" % (class_uri, set_query_string_parameter(query_string, "page", last_page)),
+            'method': "GET",
+            'rel': "last"
+        }
+    ]
+
+    previous_page = get_previous_page(page)
+    if previous_page:
+        item = {
+            'href': "%s?%s" % (class_uri, set_query_string_parameter(query_string, "page", previous_page)),
+            'method': "GET",
+            'rel': "prev"
+        }
+        links.append(item)
+
+    next_page = get_next_page(page, last_page)
+    if next_page:
+        item = {
+            'href': "%s?%s" % (class_uri, set_query_string_parameter(query_string, "page", next_page)),
+            'method': "GET",
+            'rel': "next"
+        }
+        links.append(item)
+    return links

208ee3f7e3029435fa55ed1cbfe1b51f076b3ace
icaromedeiros <icaro.medeiros@gmail.com>
2013-03-20 18:38:44 -0300

    fix in json for links section highlight

--- src/brainiak/settings.py
+++ src/brainiak/settings.py
@@ -1,20 +1,18 @@
-# WARNING: THIS FILE REFFERS ONLY TO LOCAL SERVICES
 import logging
+import os
 
 DEBUG = False
-ENVIRONMENT = "local"
+ENVIRONMENT = 'dev'
 
-SERVER_PORT = 5100
-
-SPARQL_ENDPOINT = "http://localhost:8890/sparql-auth"
-SPARQL_ENDPOINT_USER = "api-semantica"
-SPARQL_ENDPOINT_PASSWORD = "api-semantica"
+SPARQL_ENDPOINT = 'http://dev.virtuoso.globoi.com:8890/sparql-auth'
+SPARQL_ENDPOINT_USER = os.environ['GLB_API_SEMANTICA_VIRTUOSO_USR']
+SPARQL_ENDPOINT_PASSWORD = os.environ['GLB_API_SEMANTICA_VIRTUOSO_PWD']
 SPARQL_ENDPOINT_AUTH_MODE = "digest"
 SPARQL_ENDPOINT_REALM = "SPARQL"
 
 DEFAULT_LANG = "pt"
 
-LOG_FILEPATH = '/tmp/brainiak.log'
+LOG_FILEPATH = "/opt/logs/brainiak/gunicorn-be/gunicorn-be.log"
 LOG_LEVEL = logging.DEBUG
 LOG_NAME = "brainiak"
 

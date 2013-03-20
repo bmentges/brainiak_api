2a85882dcc70bcb41e516d78c2948e1cba59ba5b
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2013-03-20 15:43:57 -0300

    Add docstrings to brainiak.utils.sparql

--- src/brainiak/utils/sparql.py
+++ src/brainiak/utils/sparql.py
@@ -2,6 +2,17 @@
 
 
 def get_one_value(result_dict, key):
+    """
+    Return first value mapped by 'key' inside the 'bindings' list of a Virtuoso response dict.
+
+    Usage:
+
+    >>> result_dict = {'results': {'bindings': [{'key': {'type': 'some type', 'value': 'some value'}}]}}
+    >>> get_one_value(result_dict, 'key')
+    "some value"
+    >>> get_one_value(result_dict, 'inexistent_key')
+    False
+    """
     values = filter_values(result_dict, key)
     if not values:
         return False
@@ -9,19 +20,45 @@ def get_one_value(result_dict, key):
 
 
 def filter_values(result_dict, key):
+    """
+    Return a list of values mapped by 'key' inside the 'bindings' list of a Virtuoso response dict.
+
+    Usage:
+
+    >>> result_dict = {'results': {'bindings': [{'key': {'type': 'some type', 'value': 'some value'}}, {'key': {'type': 'some type', 'value': 'another value'}}]}}
+    >>> filter_values(result_dict, 'key')
+    ["some value", "another value"]
+    >>> filter_values(result_dict, 'inexistent_key')
+    []
+    """
     return [item[key]['value'] for item in result_dict['results']['bindings'] if item.get(key)]
 
 
-def compress_keys_and_values(result_dict, keymap={"label": "title", "subject": "@id"}, ignore_keys=["total"]):
+def compress_keys_and_values(result_dict, keymap={"label": "title", "subject": "@id"}, ignore_keys=["total"], context=None):
+    """
+    Return a list of compacted items of the 'bindings' list of a Virtuoso response dict.
+
+    Usage:
+
+    >>> result_dict = {'results': {'bindings': [{'key': {'type': 'some type', 'value': 'some value'}}, {'key': {'type': 'some type', 'value': 'another value'}}]}}
+    >>> compress_keys_and_values(result_dict)
+    [{'key': 'some value'}, {'key': 'another value'}]
+    """
     result_list = []
     for item in result_dict['results']['bindings']:
         row = {}
         for key in item:
             if not key in ignore_keys:
-                row[keymap.get(key, key)] = item[key]['value']
+                value = item[key]['value']
+                if item[key]['type'] == 'uri' and context:
+                    value = context.shorten_uri(value)
+                row[keymap.get(key, key)] = value
         result_list.append(row)
     return result_list
 
 
 def is_result_empty(result_dict):
-    return len(result_dict['results']['bindings']) == 0
+    """
+    Return True if result_dict['results']['bindings'] has no items, False otherwise.
+    """
+    return not result_dict['results']['bindings']

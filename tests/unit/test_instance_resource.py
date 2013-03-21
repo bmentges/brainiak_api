0ce42cf166dc66fca482fa11d79fccf1822322d6
rodsenra <rodsenra@gmail.com>
2013-03-21 16:41:37 -0300

    Add test case to get_instance

---
+++ tests/unit/test_instance_resource.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+import json
+import unittest
+from mock import Mock
+
+from brainiak.instance import get_resource
+from tests import MockRequest, MockResponse
+
+#         {"p": {"type": "uri", "value": "http://www.w3.org/1999/02/22-rdf-syntax-ns#type"},
+#          "o": {"type": "uri", "value": "http://semantica.globo.com/place/Country"}},
+#         {"p": {"type": "uri", "value": "http://www.w3.org/2000/01/rdf-schema#label"},
+#          "o": {"type": "literal", "xml:lang": "en", "value": "Brazil"}},
+#         {"p": {"type": "uri", "value": "http://www.w3.org/2000/01/rdf-schema#label"},
+#          "o": {"type": "literal", "xml:lang": "pt", "value": "Brasil"}},
+#         {"p": {"type": "uri", "value": "http://www.w3.org/2000/01/rdf-schema#comment"},
+#          "o": {"type": "literal", "xml:lang": "pt", "value": "Representa o pa\u00EDs Brasil."}}]}
+# })
+
+
+class TestCaseInstanceResource(unittest.TestCase):
+    def setUp(self):
+        self.original_query_all_properties_and_objects = get_resource.query_all_properties_and_objects
+        self.original_assemble_instance_json = get_resource.assemble_instance_json
+
+    def tearDown(self):
+        get_resource.query_all_properties_and_objects = self.original_query_all_properties_and_objects
+        get_resource.assemble_instance_json = self.original_assemble_instance_json
+
+    def test_get_instance_with_result(self):
+        db_response = MockResponse({"results": {"bindings": ["not_empty"]}})
+
+        def mock_query_all_properties_and_objects(context_name, class_name, instance_id):
+            return db_response
+        get_resource.query_all_properties_and_objects = mock_query_all_properties_and_objects
+
+        mock_assemble_instance_json = Mock(return_value="ok")
+        get_resource.assemble_instance_json = mock_assemble_instance_json
+        query_params = {'request': MockRequest(instance="instance"),
+                        'context_name': 'place',
+                        'class_name': 'Country',
+                        'instance_id': 'Brazil'}
+
+        response = get_resource.get_instance(query_params)
+
+        self.assertEqual(response, "ok")
+        self.assertTrue(mock_assemble_instance_json.called)
+
+    def test_get_instance_without_result(self):
+        db_response = MockResponse({"results": {"bindings": []}})
+
+        def mock_query_all_properties_and_objects(context_name, class_name, instance_id):
+            return db_response
+        get_resource.query_all_properties_and_objects = mock_query_all_properties_and_objects
+
+        mock_assemble_instance_json = Mock(return_value="ok")
+        get_resource.assemble_instance_json = mock_assemble_instance_json
+        query_params = {'request': MockRequest(instance="instance"),
+                        'context_name': 'place',
+                        'class_name': 'Country',
+                        'instance_id': 'Brazil'}
+
+        response = get_resource.get_instance(query_params)
+
+        self.assertEqual(response, None)
+        self.assertFalse(mock_assemble_instance_json.called)
+
+
+class AssembleTestCase(unittest.TestCase):
+
+    def test_assemble_instance_json_links(self):
+        query_params = {'request': MockRequest(instance="instance"), 'context_name': 'ctx', 'class_name': 'klass'}
+        query_result_dict = {'results': {'bindings': []}}
+        get_resource.build_items_dict = lambda context, bindings: {}
+        computed = get_resource.assemble_instance_json(query_params, query_result_dict)
+        expected_links = [
+            {'rel': 'self', 'href': 'http://localhost:5100/ctx/klass/instance'},
+            {'rel': 'describedBy', 'href': 'http://localhost:5100/ctx/klass/_schema'},
+            {'rel': 'edit', 'href': 'http://localhost:5100/ctx/klass/instance', 'method': 'PATCH'},
+            {'rel': 'delete', 'href': 'http://localhost:5100/ctx/klass/instance', 'method': 'DELETE'},
+            {'rel': 'replace', 'href': 'http://localhost:5100/ctx/klass/instance', 'method': 'PUT'}
+        ]
+
+        self.assertEqual(computed["@id"], "http://localhost:5100/ctx/klass/instance")
+        self.assertEqual(computed["@type"], "ctx:klass")
+        self.assertEqual(computed["@context"], {})
+        self.assertEqual(computed["$schema"], 'http://localhost:5100/ctx/klass/_schema')
+        self.assertEqual(computed["links"], expected_links)
+
+    def test_assemble_instance_json_links_with_context(self):
+
+        class InnerContextMock():
+            pass
+
+        class ContextMock():
+            context = InnerContextMock()
+            object_properties = {"person": "person:Person"}
+
+        context = ContextMock()
+        query_params = {'request': MockRequest(instance="instance"), 'context_name': 'ctx', 'class_name': 'klass'}
+        query_result_dict = {'results': {'bindings': []}}
+        get_resource.build_items_dict = lambda context, bindings: {}
+
+        computed = get_resource.assemble_instance_json(query_params, query_result_dict, context)
+        expected_links = [
+            {'rel': 'self', 'href': 'http://localhost:5100/ctx/klass/instance'},
+            {'rel': 'describedBy', 'href': 'http://localhost:5100/ctx/klass/_schema'},
+            {'rel': 'edit', 'href': 'http://localhost:5100/ctx/klass/instance', 'method': 'PATCH'},
+            {'rel': 'delete', 'href': 'http://localhost:5100/ctx/klass/instance', 'method': 'DELETE'},
+            {'rel': 'replace', 'href': 'http://localhost:5100/ctx/klass/instance', 'method': 'PUT'},
+            {'rel': 'person', 'href': '/person/Person'}
+        ]
+
+        self.assertEqual(computed["@id"], "http://localhost:5100/ctx/klass/instance")
+        self.assertEqual(computed["@type"], "ctx:klass")
+        self.assertIsInstance(computed["@context"], InnerContextMock)
+        self.assertEqual(computed["$schema"], 'http://localhost:5100/ctx/klass/_schema')
+        self.assertEqual(sorted(computed["links"]), sorted(expected_links))

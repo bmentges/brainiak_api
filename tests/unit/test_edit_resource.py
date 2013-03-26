26445eeaa589168fb2fef0eaf76714d697b63618
rodsenra <rodsenra@gmail.com>
2013-03-26 16:15:05 -0300

    Increasing test coverage

--- tests/unit/test_edit_resource.py
+++ tests/unit/test_edit_resource.py
@@ -1,9 +1,28 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+from tornado.web import HTTPError
+from brainiak.instance import edit_resource
 
 
 class TestCaseInstanceResource(unittest.TestCase):
 
-    def test_edit_instance_without_graph_uri(self):
-        pass
+    def setUp(self):
+        self.original_instance_exists = edit_resource.instance_exists
+        edit_resource.instance_exists = lambda x: True
+
+    def tearDown(self):
+        edit_resource.instance_exists = self.original_instance_exists
+
+    def test_edit_instance_without_params(self):
+        query_params = {}
+        self.assertRaises(HTTPError, edit_resource.edit_instance, query_params, None)
+
+    def test_edit_instance_with_just_instance_uri(self):
+        query_params = {'instance_uri': 'anything'}
+        self.assertRaises(HTTPError, edit_resource.edit_instance, query_params, None)
+
+    def test_edit_instance_without_class_uri(self):
+        query_params = {'instance_uri': 'anything', 'graph_uri': 'anything'}
+        self.assertRaises(HTTPError, edit_resource.edit_instance, query_params, None)
+

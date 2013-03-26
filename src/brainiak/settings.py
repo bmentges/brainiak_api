e491cb7a891adab4e71579c26102271821b19fe9
rodsenra <rodsenra@gmail.com>
2013-03-26 13:42:19 -0300

    restoring settings.py to local settings

--- src/brainiak/settings.py
+++ src/brainiak/settings.py
@@ -1,12 +1,11 @@
-# WARNING: THIS FILE REFFERS ONLY TO LOCAL SERVICES
 import logging
 
 DEBUG = False
-ENVIRONMENT = "local"
+ENVIRONMENT = 'local'
 
 SERVER_PORT = 5100
 
-SPARQL_ENDPOINT = "http://localhost:8890/sparql-auth"
+SPARQL_ENDPOINT = 'http://localhost:8890/sparql-auth'
 SPARQL_ENDPOINT_USER = "api-semantica"
 SPARQL_ENDPOINT_PASSWORD = "api-semantica"
 SPARQL_ENDPOINT_AUTH_MODE = "digest"
@@ -14,7 +13,7 @@ SPARQL_ENDPOINT_REALM = "SPARQL"
 
 DEFAULT_LANG = "pt"
 
-LOG_FILEPATH = '/tmp/brainiak.log'
+LOG_FILEPATH = "/tmp/brainiak.log"
 LOG_LEVEL = logging.DEBUG
 LOG_NAME = "brainiak"
 

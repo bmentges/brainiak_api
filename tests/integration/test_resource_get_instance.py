f9572242e5fef737f5274f2b9af2365f3c4b975b
icaromedeiros <icaro.medeiros@gmail.com>
2013-03-27 18:53:36 -0300

    fixing tests -- not depending on local ontology data for get instance

--- tests/integration/test_resource_get_instance.py
+++ tests/integration/test_resource_get_instance.py
@@ -2,15 +2,20 @@ import json
 from mock import patch
 
 from tests import TornadoAsyncHTTPTestCase
+from tests.sparql import QueryTestCase
 
 
-class InstanceResourceTestCase(TornadoAsyncHTTPTestCase):
+class InstanceResourceTestCase(TornadoAsyncHTTPTestCase, QueryTestCase):
+
+    allow_triplestore_connection = True
+    fixtures = ["tests/sample/gender.n3"]
+    graph_uri = "http://test.com/"
 
     maxDiff = None
 
     @patch("brainiak.handlers.log")
     def test_get_instance_400(self, log):
-        response = self.fetch('/place/City/Tatilandia?eh=bigoletinha', method='GET')
+        response = self.fetch('/person/Gender/Female?eh=bigoletinha', method='GET')
         self.assertEqual(response.code, 400)
 
     @patch("brainiak.handlers.log")
@@ -29,21 +34,11 @@ class InstanceResourceTestCase(TornadoAsyncHTTPTestCase):
         self.assertItemsEqual(body['rdfs:label'], [u'Female', u'Feminino'])
 
     def test_get_instance_with_compressed_instance_prefix_200(self):
-        response = self.fetch('/place/City/Cidade_Uberaba_MG?instance_prefix=base', method='GET')
+        response = self.fetch('/person/Gender/Test?instance_prefix=http://test.com/other_prefix/&class_uri=http://test.com/person/Gender', method='GET')
         body = json.loads(response.body)
         self.assertEqual(response.code, 200)
-        self.assertIn(u'/place/City/_schema', body['$schema'])
-        self.assertIn(u'/place/City/Cidade_Uberaba_MG?instance_prefix=base', body['@id'])
-        self.assertEqual(body['@type'], u'place:City')
-        self.assertItemsEqual(body['rdf:type'], [u'place:City', u'base:Cidade'])
-        self.assertEqual(body['rdfs:label'], u'Uberaba')
-
-    def test_get_instance_with_expanded_instance_prefix_200(self):
-        response = self.fetch('/place/City/Cidade_Uberaba_MG?instance_prefix=http://semantica.globo.com/base/', method='GET')
-        body = json.loads(response.body)
-        self.assertEqual(response.code, 200)
-        self.assertIn(u'/place/City/_schema', body['$schema'])
-        self.assertIn(u'/place/City/Cidade_Uberaba_MG?instance_prefix=http://semantica.globo.com/base/', body['@id'])
-        self.assertEqual(body['@type'], u'place:City')
-        self.assertItemsEqual(body['rdf:type'], [u'place:City', u'base:Cidade'])
-        self.assertEqual(body['rdfs:label'], u'Uberaba')
+        self.assertIn(u'/person/Gender/_schema', body['$schema'])
+        self.assertIn(u'/person/Gender/Test?instance_prefix=http://test.com/other_prefix/', body['@id'])
+        self.assertEqual(body['@type'], u'person:Gender')
+        self.assertEqual(body['rdf:type'], u'http://test.com/person/Gender')
+        self.assertEqual(body['rdfs:label'], u'Teste')

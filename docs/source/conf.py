1fc26fc49ccc59a3f6278d588cd70a818710239b
rodsenra <rodsenra@gmail.com>
2013-03-27 14:50:30 -0300

    Zero warnings for sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -67,7 +67,8 @@ release = '0.0.1'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = []
+exclude_patterns = ['services/instance/examples/*.rst',
+                    'services/schema/examples/*.rst']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
@@ -125,7 +126,7 @@ html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.

18bc783ae3bd87247f3736981cd973045b9f8b14
icaromedeiros <icaro.medeiros@gmail.com>
2013-04-04 14:36:12 -0300

    tested query

--- src/brainiak/context/list_resource.py
+++ src/brainiak/context/list_resource.py
@@ -1,5 +1,7 @@
 from brainiak.utils.sparql import add_language_support, is_result_empty
 from brainiak import triplestore
+from brainiak.utils.sparql import compress_keys_and_values
+from brainiak.prefixes import MemorizeContext
 
 
 def list_classes(query_params):
@@ -10,8 +12,22 @@ def list_classes(query_params):
         return assemble_list_json(query_result_dict)
 
 
-def assemble_list_json():
-    pass
+def assemble_list_json(query_result_dict):
+    context = MemorizeContext()
+    json_dict = compress_keys_and_values(query_result_dict,
+                                         keymap={"class": "@id", "label": "title"},
+                                         context=context)
+#    links_section = get_links()
+#    links = build_links(
+#        class_url,
+#        page=int(query_params["page"]) + 1,  # API's pagination begin with 1, Virtuoso's with 0
+#        per_page=int(query_params["per_page"]),
+#        request_uri=request.uri,
+#        total_items=total_items,
+#        query_string=request.query)
+
+
+    json_dict.extend(links_section)
 
 
 QUERY_ALL_CLASSES_OF_A_GRAPH = """

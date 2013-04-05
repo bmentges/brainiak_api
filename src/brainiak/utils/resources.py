a744e12026992ca57b41cebab8a50f66dc90b72e
rodsenra <rodsenra@gmail.com>
2013-04-05 14:18:06 -0300

    Fix bug of resource_id no filled right when @id with curie

--- src/brainiak/utils/resources.py
+++ src/brainiak/utils/resources.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from brainiak.prefixes import expand_uri
 
 
 def decorate_with_resource_id(list_of_dicts):
@@ -6,7 +7,7 @@ def decorate_with_resource_id(list_of_dicts):
     a new key`resource_id` created from the last path segment given by the key `@id`"""
     for dict_item in list_of_dicts:
         try:
-            id_key = dict_item["@id"]
+            id_key = expand_uri(dict_item["@id"])
             dict_item['resource_id'] = id_key.rsplit("/")[-1]
         except KeyError:
             pass

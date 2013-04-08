c16e40dab4ec4dd110afbdd4152f04783406b34e
rodsenra <rodsenra@gmail.com>
2013-04-08 16:29:25 -0300

    Adds utils/client.py module with test cases and full coverage. Adds first acceptance test

---
+++ tests/acceptance/test_contexts.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from unittest import TestCase
+from brainiak.utils.client import fetch_all_pages, extract_keys
+
+
+API_ENDPOINT = "http://api.semantica.dev.globoi.com/v2"
+
+
+class AcceptListContexts(TestCase):
+
+    def test_successful_list_context(self):
+        response_items = fetch_all_pages(API_ENDPOINT, 'items')
+        titles = extract_keys(response_items, "title")
+        self.assertIn(u'glb', titles)
+        self.assertIn(u'base', titles)
+        self.assertIn(u'upper', titles)
+        self.assertIn(u'place', titles)
+        self.assertIn(u'person', titles)
+        self.assertIn(u'organization', titles)

4429f54477782cb71e0666a22ec91c82105a1087
rodsenra <rodsenra@gmail.com>
2013-04-08 18:05:44 -0300

    Refactoring domains -> contexts. Increase test coverage

--- tests/unit/test_list_contexts.py
+++ tests/unit/test_list_contexts.py
@@ -1,7 +1,8 @@
 import unittest
+from tornado.web import HTTPError
 
 from brainiak import triplestore
-from brainiak.root.get import filter_and_build_domains, build_json, list_domains
+from brainiak.root.get import filter_and_build_contexts, build_json, list_all_contexts
 from brainiak.root import get
 from brainiak.utils import sparql
 from tests import MockRequest
@@ -10,25 +11,36 @@ from tests import MockRequest
 class MockedTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.original_filter_and_build_domains = get.filter_and_build_domains
-
-    def tearDown(self):
-        get.filter_and_build_domains = self.original_filter_and_build_domains
-
-
-class GetDomainTestCase(unittest.TestCase):
-
-    maxDiff = None
-
-    def setUp(self):
+        self.original_filter_and_build_contexts = get.filter_and_build_contexts
         self.original_filter_values = sparql.filter_values
         self.original_query_sparql = triplestore.query_sparql
 
     def tearDown(self):
+        get.filter_and_build_contexts = self.original_filter_and_build_contexts
         sparql.filter_values = self.original_filter_values
         triplestore.query_sparql = self.original_query_sparql
 
-    def test_list_domains(self):
+    def test_raises_http_error(self):
+        triplestore.query_sparql = lambda query: None
+        sparql.filter_values = lambda a, b: []
+
+        def mock_filter_and_build_contexts(contexts_uris):
+            return []
+        get.filter_and_build_contexts = mock_filter_and_build_contexts
+        self.assertRaises(HTTPError, list_all_contexts, 'irrelevant_params', 'irrelevant_request')
+
+
+class GetContextTestCase(unittest.TestCase):
+
+    maxDiff = None
+
+    def setUp(self):
+        self.original_query_sparql = triplestore.query_sparql
+
+    def tearDown(self):
+        triplestore.query_sparql = self.original_query_sparql
+
+    def test_list_contexts(self):
         response = {
             "results":
                 {"bindings": [
@@ -40,7 +52,7 @@ class GetDomainTestCase(unittest.TestCase):
         params = {"per_page": "30", "page": "0"}
         request = MockRequest()
         request.uri = "http://api.semantica.dev.globoi.com/v2/"
-        computed = list_domains(params, request)
+        computed = list_all_contexts(params, request)
         expected_items = [
             {'@id': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
              'title': 'rdf',
@@ -63,12 +75,12 @@ class GetDomainTestCase(unittest.TestCase):
         ]
         self.assertEqual(computed["links"], expected_links)
 
-    def test_build_domains_that_exist_in_prefixes(self):
-        domains_uris = [
+    def test_build_contexts_that_exist_in_prefixes(self):
+        contexts_uris = [
             "http://www.w3.org/2006/time#",
             'http://xmlns.com/foaf/0.1/'
         ]
-        computed = filter_and_build_domains(domains_uris)
+        computed = filter_and_build_contexts(contexts_uris)
         expected = [
             {'@id': 'http://www.w3.org/2006/time#',
              'title': 'time',
@@ -79,13 +91,13 @@ class GetDomainTestCase(unittest.TestCase):
         ]
         self.assertEqual(computed, expected)
 
-    def test_build_domains_of_which_one_doesnt_exist_in_prefixes(self):
-        domains_uris = [
+    def test_build_contexts_of_which_one_doesnt_exist_in_prefixes(self):
+        contexts_uris = [
             'http://purl.org/dc/elements/1.1/',
             'http://dbpedia.org/ontology/',
             'http://unregistered.prefix'
         ]
-        computed = filter_and_build_domains(domains_uris)
+        computed = filter_and_build_contexts(contexts_uris)
         expected = [
             {'@id': 'http://purl.org/dc/elements/1.1/',
              'title': 'dc',
@@ -97,12 +109,12 @@ class GetDomainTestCase(unittest.TestCase):
         self.assertEqual(computed, expected)
 
     def test_build_json(self):
-        domains = ["a", "b", "c"]
+        contexts = ["a", "b", "c"]
         params = {"per_page": "3", "page": "0"}
         total_items = 6
         request = MockRequest()
         request.uri = 'http://localhost:5100/'
-        computed = build_json(domains, total_items, params, request)
+        computed = build_json(contexts, total_items, params, request)
         self.assertEqual(computed['items'], ["a", "b", "c"])
         self.assertEqual(computed['item_count'], 6)
         expected_links = [

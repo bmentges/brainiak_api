8f22a4c1b1fec51f329ed64f06801fd375a702fa
rodsenra <rodsenra@gmail.com>
2013-04-22 16:04:55 -0300

    Add schema= for rel=replace on instances IssueID #9977 @00:30

--- tests/unit/test_instance_resource.py
+++ tests/unit/test_instance_resource.py
@@ -86,7 +86,7 @@ class AssembleTestCase(unittest.TestCase):
             {'rel': 'describedBy', 'href': 'http://mock.test.com/schema/klass/_schema', 'method': 'GET'},
             {'rel': 'inCollection', 'href': 'http://mock.test.com/schema/klass', 'method': 'GET'},
             {'rel': 'delete', 'href': 'http://mock.test.com/schema/klass/instance', 'method': 'DELETE'},
-            {'rel': 'replace', 'href': 'http://mock.test.com/schema/klass/instance', 'method': 'PUT'}
+            {'rel': 'replace', 'href': 'http://mock.test.com/schema/klass/instance', 'method': 'PUT', 'schema': {'$ref': 'http://mock.test.com/schema/klass/_schema'}}
         ]
 
         self.assertEqual(computed["@id"], "http://schema.org/klass/instance")
@@ -119,7 +119,7 @@ class AssembleTestCase(unittest.TestCase):
             {'rel': 'describedBy', 'href': 'http://mock.test.com/schema/klass/_schema', 'method': 'GET'},
             {'rel': 'inCollection', 'href': 'http://mock.test.com/schema/klass', 'method': 'GET'},
             {'rel': 'delete', 'href': 'http://mock.test.com/schema/klass/instance', 'method': 'DELETE'},
-            {'rel': 'replace', 'href': 'http://mock.test.com/schema/klass/instance', 'method': 'PUT'},
+            {'rel': 'replace', 'href': 'http://mock.test.com/schema/klass/instance', 'method': 'PUT', 'schema': {'$ref': 'http://mock.test.com/schema/klass/_schema'}},
             {'rel': 'person', 'href': '/person/Person'}
         ]
 

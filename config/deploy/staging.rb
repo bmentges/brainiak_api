f1856632be792fc49a8cc753aeccdbecfd37fac8
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2013-04-24 14:25:00 -0300

    Add event bus configs to all envs (.rb) and settings.py.unfiltered

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,7 +5,8 @@ role :docs, "riovlb160.globoi.com", :no_release => true
 
 set :sparql_endpoint, "http://staging.semantica.globoi.com:8890/sparql-auth"
 
-
+set :event_bus_host, "barramento.baas.globoi.com"
+set :event_bus_port, "61613"
 
 
 # Used by puppet module in Capistrano

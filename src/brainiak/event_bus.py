fdd5c801a92cff099474dd4f2513bf8f17b32b20
icaromedeiros <icaro.medeiros@gmail.com>
2013-05-06 18:44:59 -0300

    refactoring on eventu bus connection, reconnection, etc. issue #9956 03:00h

--- src/brainiak/event_bus.py
+++ src/brainiak/event_bus.py
@@ -17,6 +17,7 @@ event_bus_connection = stomp.Connection(host_and_ports=[(EVENT_BUS_HOST, EVENT_B
 
 
 def notify_bus(instance, klass, graph, action):
+    global event_bus_connection
     notifiable_dict = {
         "instance": instance,
         "class": klass,
@@ -26,18 +27,25 @@ def notify_bus(instance, klass, graph, action):
     message = json.dumps(notifiable_dict)
     logger.info("BUS NOTIFICATION\n%s" % message)
     try:
+        if not event_bus_connection.is_connected():
+            reconnect()
         event_bus_connection.send(message, destination=EVENT_BUS_QUEUES)
     except (ConnectionClosedException, NotConnectedException, ProtocolException) as e:
-        logger.warn("ActiveMQ unavailable due to %s." % str(e.__class__))
-        # try:
-        #     reconnect()
-        # except (ConnectionClosedException, NotConnectedException) as e:
-        #     raise Exception("Error when notifying event bus. Type: " + str(e.__class__))
-        # else:
-        notify_bus(instance, klass, graph, action)
+        logger.error("ActiveMQ unavailable due to %s." % str(e.__class__))
+        raise EventBusException()
+
+
+def initialize_event_bus_connection():
+    global event_bus_connection
+    try:
+        event_bus_connection.start()
+        event_bus_connection.connect()
+    except:
+        logger.warn("Event bus connection went wrong.\n")
 
 
 def status(host=EVENT_BUS_HOST, port=EVENT_BUS_PORT):
+    global event_bus_connection
     try:
         event_bus_connection.abort({'transaction': '<ping_transaction>'})
     except (ConnectionClosedException, NotConnectedException, ProtocolException) as e:
@@ -51,5 +59,9 @@ def status(host=EVENT_BUS_HOST, port=EVENT_BUS_PORT):
 
 def reconnect(host=EVENT_BUS_HOST, port=EVENT_BUS_PORT):
     logger.info("Trying to reconnect to ActiveMQ at %s:%d..." % (host, port))
-    global event_bus_connection
-    event_bus_connection = stomp.Connection(host_and_ports=[(EVENT_BUS_HOST, EVENT_BUS_PORT)])
+    initialize_event_bus_connection()
+
+
+class EventBusException(Exception):
+
+    pass

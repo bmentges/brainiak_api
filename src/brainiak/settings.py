db8c3043ada682c83c8344e98a0c486714d5938d
icaromedeiros <icaro.medeiros@gmail.com>
2013-05-08 18:39:10 -0300

    bus notification can be turned off in settings

--- src/brainiak/settings.py
+++ src/brainiak/settings.py
@@ -4,8 +4,6 @@ import logging
 DEBUG = True
 ENVIRONMENT = 'local'
 
-SERVER_PORT = 5100
-
 SPARQL_ENDPOINT = 'http://localhost:8890/sparql-auth'
 SPARQL_ENDPOINT_USER = "api-semantica"
 SPARQL_ENDPOINT_PASSWORD = "api-semantica"
@@ -14,16 +12,19 @@ SPARQL_ENDPOINT_REALM = "SPARQL"
 
 DEFAULT_LANG = "pt"
 
+DEFAULT_PER_PAGE = "10"
+DEFAULT_PAGE = "0"
+
 LOG_FILEPATH = "/tmp/brainiak.log"
 LOG_LEVEL = logging.DEBUG
 LOG_NAME = "brainiak"
 
 URI_PREFIX = "http://semantica.globo.com/"
 
-DEFAULT_PER_PAGE = "10"
-DEFAULT_PAGE = "0"
-
 EVENT_BUS_HOST = "localhost"
 EVENT_BUS_PORT = 61613
+NOTIFY_BUS = False
 
 CORS_HEADERS = 'Content-Type, Authorization'
+
+SERVER_PORT = 5100

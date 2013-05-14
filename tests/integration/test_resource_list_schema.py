cd524a819f2e0ab085a3d2fdf8085482563823b4
rodsenra <rodsenra@gmail.com>
2013-05-14 10:37:49 -0300

    Fix minItems==1 -> generating required: True to work with CMAaS

--- tests/integration/test_resource_list_schema.py
+++ tests/integration/test_resource_list_schema.py
@@ -561,7 +561,7 @@ class GetPredicatesCardinalitiesTestCase(TornadoAsyncTestCase):
                 'graph': 'http://test/person/',
                 'format': 'uri',
                 'maxItems': '1',
-                'minItems': '1',
+                'required': True,
                 'type': 'string',
                 'range': {'graph': 'http://test/person/',
                           '@id': 'http://test/person/Gender',

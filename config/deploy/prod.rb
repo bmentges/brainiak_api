0fc09454e99bfe23273afa43eb0c1779e1251348
Joao Carlos Mendes Luis <jonny@jonny.eng.br>
2013-05-27 15:28:19 -0300

    Faz a interpolacao de porta do sparql em ruby/cap, para evitar problemas futuros com a sintaxe

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -3,8 +3,8 @@ role :app, "filer.globoi.com" # deploys app source to filer
 role :restart, "riomp40lb14.globoi.com","riomp41lb14.globoi.com", :no_release => true # application
 role :docs, "riomp40lb14.globoi.com", :no_release => true
 
-set :sparql_port, "80"
-set :sparql_endpoint, "http://virtuoso.semantica.globoi.com:%s/api-semantica/sparql-auth"
+set :sparql_port, 80
+set :sparql_endpoint, "http://virtuoso.semantica.globoi.com:#{sparql_port}/api-semantica/sparql-auth"
 
 set :event_bus_host, "barramento.baas.globoi.com"
 
--- config/deploy/qa.rb
+++ config/deploy/qa.rb
@@ -3,8 +3,8 @@ role :app, "filer.qa.globoi.com"  # deploys app source to filer
 role :restart, "cittavld507.globoi.com", "cittavld797.globoi.com", :no_release => true # application
 role :docs, "cittavld507.globoi.com", :no_release => true
 
-set :sparql_port, "8890"
-set :sparql_endpoint, "http://qa.virtuoso.globoi.com:%s/sparql-auth"
+set :sparql_port, 8890
+set :sparql_endpoint, "http://qa.virtuoso.globoi.com:#{sparql_port}/sparql-auth"
 
 set :event_bus_host, "barramento.baas.qa.globoi.com"
 

87d23645265013063e34873486a962525693f77f
rodsenra <rodsenra@gmail.com>
2013-05-28 15:53:37 -0300

    Fix sparql port for qa1

--- config/deploy/qa1.rb
+++ config/deploy/qa1.rb
@@ -3,7 +3,7 @@ role :app, "filer.qa01.globoi.com"  # deploys app source to filer
 role :restart, "cittavld45.globoi.com", :no_release => true # application
 role :docs, "cittavld45.globoi.com", :no_release => true
 
-set :sparql_port, 80
+set :sparql_port, 8890
 set :sparql_endpoint, "http://qa1.virtuoso.globoi.com:#{sparql_port}/sparql-auth"
 
 set :event_bus_host, "barramento.baas.qa01.globoi.com"

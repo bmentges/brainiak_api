9b96ee4d72103112f8b0cdb4bf3308295844bcc2
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2013-05-28 18:58:37 -0300

    Interactive examples

---
+++ tests/interactive/delete_new_city_prod.sh
@@ -0,0 +1,3 @@
+curl -i -X DELETE -T "new_city.json" http://api.semantica.globoi.com/place/City/dc90ef7f-69ec-497b-8d61-139ba8e826cd
+#http://api.semantica.globoi.com/place/City/825b73bd-1b3e-4734-b084-63a4d08852d1
+#http://api.semantica.globoi.com/place/City/acfa4231-5815-409b-8cf7-37e757829890

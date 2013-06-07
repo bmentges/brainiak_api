72772e71973cb80464e3882746d2108092748fd6
rodsenra <rodsenra@gmail.com>
2013-06-07 11:19:28 -0300

    Fix notify_bus mock in unit test cases

--- tests/unit/test_event_bus.py
+++ tests/unit/test_event_bus.py
@@ -12,20 +12,19 @@ class EventBusTestCase(TestCase):
     @patch("brainiak.event_bus.logger")
     @patch("brainiak.event_bus.settings", NOTIFY_BUS=True)
     @patch("dad.mom.Middleware.__init__", side_effect=MiddlewareError("mocked failure"))
-    def test_initialize_raises_exception(self, mocked_init, mocked_logger, mocked_settings):
+    def test_initialize_raises_exception(self, mocked_init, mocked_settings, mocked_logger):
         self.assertRaises(NotificationFailure, event_bus.initialize)
 
-    @patch("brainiak.event_bus.settings", NOTIFY_BUS=True)
     @patch("brainiak.event_bus.logger")
-    @patch("dad.mom.Middleware.notify")
-    def test_notify_bus(self, mocked_middleware_notify, mocked_logger, mocked_settings):
-        event_bus.initialize()
+    @patch("brainiak.event_bus.settings", NOTIFY_BUS=True)
+    @patch("brainiak.event_bus.middleware")
+    def test_notify_bus(self, mocked, mocked_settings, mocked_logger):
         event_bus.notify_bus(action="1", klass="2", graph="3", instance="4")
-        self.assertTrue(mocked_middleware_notify.called)
+        self.assertTrue(mocked.notify.called)
 
     @patch("brainiak.event_bus.logger")
-    @patch("dad.mom.Middleware.notify", side_effect=MiddlewareError("mocked failure"))
     @patch("brainiak.event_bus.settings", NOTIFY_BUS=True)
-    def test_notify_raises_exception(self, mocked_middleware_notify, mocked_notify_bus, mocked_logger):
-        event_bus.initialize()
+    @patch("brainiak.event_bus.middleware.notify", side_effect=MiddlewareError("mocked failure"))
+    @patch("brainiak.event_bus.middleware")
+    def test_notify_raises_exception(self, mocked_middleware, mocked_notify, mocked_settings, mocked_logger):
         self.assertRaises(NotificationFailure, event_bus.notify_bus, action="1", klass="2", graph="3", instance="4")

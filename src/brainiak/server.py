32d5425aaed2a438dd40c20b9cc58eef51206371
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2013-06-07 10:39:37 -0300

    Parse command line args in a tornado-like manner (tornado.options stuff)

--- src/brainiak/server.py
+++ src/brainiak/server.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 import sys
+
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
+from tornado.options import define, options, parse_command_line
 from tornado.web import Application as TornadoApplication
 
 from brainiak import __doc__, log, settings
@@ -9,7 +11,10 @@ from brainiak.greenlet_tornado import greenlet_set_ioloop
 from brainiak.handlers import get_routes
 from brainiak import event_bus
 
+
 server = None
+define("port", default=settings.SERVER_PORT, help="Run app on the given port", type=int)
+define("debug", default=settings.DEBUG, help="Debug mode", type=bool)
 
 
 class Application(TornadoApplication):
@@ -20,25 +25,21 @@ class Application(TornadoApplication):
             event_bus.initialize()
             super(Application, self).__init__(get_routes(), debug=debug)
         except Exception as e:
-            print ("Failed to initialize application. {0}".format(str(e)))
+            sys.stdout.write("Failed to initialize application. {0}".format(str(e)))
             sys.exit(1)
 
 application = Application()
 
 
-def main(args):  # pragma: no cover
-    application = Application(debug=args.debug)
+def main():  # pragma: no cover
+    parse_command_line()
+    application = Application(debug=options.debug)
     server = HTTPServer(application)
-    server.listen(settings.SERVER_PORT)
+    server.listen(options.port)
     io_loop = IOLoop.instance()
     greenlet_set_ioloop(io_loop)
     io_loop.start()
 
 
 if __name__ == '__main__':
-    from argparse import ArgumentParser
-    parser = ArgumentParser(description=__doc__)
-    parser.add_argument('-d', '--debug', action='store_const', const=True, default=settings.DEBUG, help='debug mode')
-    args = parser.parse_args()
-
-    main(args)
+    main()

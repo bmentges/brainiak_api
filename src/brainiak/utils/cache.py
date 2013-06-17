f4760bc79be310c55d83be3becd3516c32d1dbff
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2013-06-17 15:57:26 -0300

    Add cache aux functions and start of purge implementation

--- src/brainiak/utils/cache.py
+++ src/brainiak/utils/cache.py
@@ -2,6 +2,7 @@ import redis
 import ujson
 
 from brainiak import settings
+from brainiak import log
 
 
 redis_server = redis.StrictRedis(host=settings.REDIS_ENDPOINT, port=settings.REDIS_PORT, password=settings.REDIS_PASSWORD, db=0)
@@ -12,10 +13,10 @@ def memoize(function):
     def wrapper(params):
         if settings.ENABLE_CACHE:
             url = params['request'].uri
-            cached_json = redis_server.get(url)
+            cached_json = retrieve(url)
             if (cached_json is None):
                 json = function(params)
-                redis_server.set(url, ujson.dumps(json))
+                create(url, ujson.dumps(json))
                 return json
             else:
                 return ujson.loads(cached_json)
@@ -26,5 +27,33 @@ def memoize(function):
 
 
 def purge(pattern):
-    keys_with_pattern = redis_server.keys(pattern)
-    redis_server.delete(keys_with_pattern)
+    keys_with_pattern = keys(pattern) or []
+    log.logger.debug("Cache: key(s) to be deleted: {0}".format(keys_with_pattern))
+    log_details = "{0} key(s), matching the pattern: {1}".format(len(keys_with_pattern), pattern)
+    response = 1
+    for key in keys_with_pattern:
+        response *= delete(key)
+
+    if response and keys_with_pattern:
+        log.logger.info("Cache: purged with success {0}".format(log_details))
+    elif not keys_with_pattern:
+        log.logger.info("Cache: {0}".format(log_details))
+    else:
+        log.logger.info("Cache: failed purging {0}".format(log_details))
+
+
+def create(key, value):
+    return redis_server.set(key, value)
+
+
+def retrieve(key):
+    return redis_server.get(url)
+
+
+def delete(keys):
+    return redis_server.delete(keys)
+
+
+def keys(pattern):
+    pattern = "{0}*".format(pattern)
+    return redis_server.keys(pattern)

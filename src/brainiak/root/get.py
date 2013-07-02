520b15e1ddc540ec0c1be81160c3590a4d276fa8
rodsenra <rodsenra@gmail.com>
2013-07-02 18:20:57 -0300

    PEP8fication. Fixing broken test cases

--- src/brainiak/root/get.py
+++ src/brainiak/root/get.py
@@ -3,7 +3,7 @@ from tornado.web import HTTPError
 from brainiak import triplestore
 from brainiak.prefixes import prefix_to_slug, STANDARD_PREFIXES
 from brainiak.utils import sparql
-from brainiak.utils.links import split_into_chunks,  self_url
+from brainiak.utils.links import split_into_chunks, self_url
 from brainiak.utils.resources import decorate_dict_with_pagination
 
 # Note that pagination was done outside the query

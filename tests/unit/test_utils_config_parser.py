1dc8faf037fdfc18777fde56f1a3eea0f643c21f
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2013-07-18 16:27:10 -0300

    Re-implemented parse_section and tests

--- tests/unit/test_utils_config_parser.py
+++ tests/unit/test_utils_config_parser.py
@@ -1,19 +1,22 @@
 from unittest import TestCase
 from mock import patch
-
 from brainiak.utils.config_parser import parse_section
-from ConfigParser import NoSectionError
+
 
 class ConfigParserTestCase(TestCase):
 
-    @patch("brainiak.utils.config_parser.ConfigParser.read")
-    @patch("brainiak.utils.config_parser.ConfigParser.options", return_value=["some_key"])
-    @patch("brainiak.utils.config_parser.ConfigParser.get", return_value="some_value")
-    def test_config_file_with_default_section(self, mocked_get, mocked_options, mocked_read):
-        expected_dict = {"some_key": "some_value"}
-        self.assertEqual(expected_dict, parse_section("filename"))
+    def test_parse_existent_section(self):
+        local_ini = "config/local/triplestore.ini"
+        response = parse_section(local_ini, "default")
+        expected_response = {
+            'endpoint': 'http://localhost:8890',
+            'realm': 'SPARQL',
+            'app_name': 'Brainiak',
+            'auth_mode': 'digest',
+            'user': 'api-semantica',
+            'password': 'api-semantica'
+        }
+        self.assertEqual(response, expected_response)
 
-    @patch("brainiak.utils.config_parser.ConfigParser.read")
-    @patch("brainiak.utils.config_parser.ConfigParser.options", side_effect=NoSectionError(""))
-    def test_config_file_no_default_section(self, mocked_options, mocked_read):
-        self.assertRaises(Exception, parse_section, "filename")
+    def test_parse_inexistent_section(self):
+        self.assertRaises(Exception, parse_section, "xubiru")

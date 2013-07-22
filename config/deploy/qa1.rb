dddb4aea2e3623ac4d8fc200b8d9f3861f9dd1ec
Diogo Kiss <diogokiss@corp.globo.com>
2013-07-22 18:39:34 -0300

    Refactoring de variaveis e implementacao de TDIs.

--- config/deploy/qa1.rb
+++ config/deploy/qa1.rb
@@ -1,12 +1,19 @@
-role :be,               'cittavld45.globoi.com'
-role :filer,            'filer.qa01.globoi.com'
+role :be,                   'cittavld45.globoi.com'
+role :filer,                'filer.qa01.globoi.com'
 
-set :sparql_port,       8890
-set :sparql_endpoint,   "http://qa1.virtuoso.globoi.com:#{sparql_port}/sparql-auth"
-set :event_bus_host,    'barramento.baas.qa01.globoi.com'
-set :puppetmaster_env,  'qa1'
-set :redis_endpoint,    'redis.qa01.globoi.com'
-set :redis_port,        20015
-set :redis_password,    'a8pdifs2e2m9afn7tcifcea99674aad2'
+# Hosts
+set :virtuoso_host,         'qa1.virtuoso.globoi.com'
+set :barramento_baas_host,  'barramento.baas.qa01.globoi.com'
+set :redis_host,            'redis.qa01.globoi.com'
+set :syslog_host,           'syslog.tcp.glog.qa01.globoi.com'
+set :filer_host,            'riofd06'
 
-set :log_filepath,      '/opt/logs/brainiak/gunicorn-be/gunicorn-be.log'
+# Ports
+set :sparql_port,           8890
+
+# URLs
+set :virtuoso_url,          "http://#{virtuoso_host}:#{sparql_port}/sparql-auth"
+
+# Variables
+set :puppetmaster_env,      'qa1'
+set :redis_password,        'a8pdifs2e2m9afn7tcifcea99674aad2'

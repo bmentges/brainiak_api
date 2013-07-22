923a703e4b9ed5e0d1552bd5dfe2cbcbd2ab3c6c
Diogo Kiss <diogokiss@corp.globo.com>
2013-07-22 18:39:34 -0300

    Refactoring de variaveis e implementacao de TDIs.

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -1,13 +1,19 @@
-role :filer,            'filer.globoi.com'
-role :be,               'riomp40lb14.globoi.com','riomp41lb14.globoi.com'
+role :be,                   'riomp40lb14.globoi.com','riomp41lb14.globoi.com'
+role :filer,                'filer.globoi.com'
 
+# Hosts
+set :virtuoso_host,         'virtuoso.semantica.globoi.com'
+set :barramento_baas_host,  'barramento.baas.globoi.com'
+set :redis_host,            'redis.api.semantica.globoi.com'
+set :syslog_host,           'syslog.tcp.glog.globoi.com'
+set :filer_host,            'riofb01a'
 
-set :sparql_port,       80
-set :sparql_endpoint,   "http://virtuoso.semantica.globoi.com:#{sparql_port}/api-semantica/sparql-auth"
-set :event_bus_host,    'barramento.baas.globoi.com'
-set :puppetmaster_env,  'prod'
-set :redis_endpoint,    'redis.api.semantica.globoi.com'
-set :redis_port,        20015
-set :redis_password,    'a8pdifs2e2m9afn7tcifcea99674aad2'
+# Ports
+set :sparql_port,           80
 
-set :log_filepath,      '/opt/logs/brainiak/gunicorn-be/gunicorn-be.log'
+# URLs
+set :virtuoso_url,          "http://#{virtuoso_host}:#{sparql_port}/sparql-auth"
+
+# Variables
+set :puppetmaster_env,      'prod'
+set :redis_password,        'a8pdifs2e2m9afn7tcifcea99674aad2'

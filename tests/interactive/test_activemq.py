08a6e59a009221243c6b768455a3792fb164ec02
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2013-07-22 15:37:21 -0300

    Add ActiveMQ simple and independent test

---
+++ tests/interactive/test_activemq.py
@@ -0,0 +1,9 @@
+import stomp
+
+host = 'barramento.baas.dev.globoi.com'
+port = 61613
+
+connection = stomp.Connection(host_and_ports=[(host, port)])
+connection.start()
+connection.connect()
+connection.begin({'message-id': 'from_dad'})

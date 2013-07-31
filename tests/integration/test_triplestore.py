1c71360856e0125b5fc0deece0ee79d22e25f028
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2013-07-31 17:55:19 -0300

    triplestore.py coverage: from 72% to 100% \o/

--- tests/integration/test_triplestore.py
+++ tests/integration/test_triplestore.py
@@ -67,12 +67,17 @@ class TriplestoreTestCase(TornadoAsyncTestCase):
         else:
             self.fail("HTTPError not raised")
 
+    @patch("brainiak.triplestore.greenlet_fetch", return_value=None)
+    @patch("brainiak.triplestore.log.logger.info")
+    @patch("brainiak.triplestore.time.time", return_value=0)
     @patch("brainiak.triplestore.settings",
            SPARQL_ENDPOINT=EndpointConfig.URL,
            SPARQL_ENDPOINT_AUTH_MODE=EndpointConfig.DIGEST,
            SPARQL_ENDPOINT_USER=EndpointConfig.USER,
            SPARQL_ENDPOINT_PASSWORD=EndpointConfig.PASSWORD)
-    @greenlet_tornado.greenlet_test
-    def test_authenticated_access_to_not_authenticated_endpoint(self, settings):
+    def test_authenticated_access_to_not_authenticated_endpoint(self, settings, time, info, greenlet_fetch):
         response = triplestore.run_query(SIMPLE_COUNT_CLASSES_QUERY)
-        self.assertEqual(response.code, 200)
+        self.assertEqual(response, None)
+        self.assertEqual(info.call_count, 1)
+        log_msg = "POST - http://localhost:8890/sparql - None - api-semantica [tempo: 0] - QUERY - SELECT COUNT(*) WHERE {?s a owl:Class}"
+        self.assertTrue(info.call_args, log_msg)

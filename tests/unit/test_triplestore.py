a0ff92fa9f1c328df99e4a3f32ed6e298fd5fa1a
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2013-07-31 17:55:19 -0300

    triplestore.py coverage: from 72% to 100% \o/

--- tests/unit/test_triplestore.py
+++ tests/unit/test_triplestore.py
@@ -2,10 +2,19 @@
 import unittest
 from mock import patch
 
-from tornado.httpclient import HTTPError
+from tornado.httpclient import HTTPError, HTTPResponse
 
 from brainiak import triplestore
 import SPARQLWrapper
+
+
+class MockException(Exception):
+    def __str__(self):
+        return "Mocked exception"
+
+
+class MockResponse(object):
+    body = "{}"
 
 
 class MockSPARQLWrapper():
@@ -80,3 +89,35 @@ class TestCaseStatus(unittest.TestCase):
         msg2 = "Virtuoso connection authenticated [USER:1\x9fM&\xe3\xc56\xb5\xdd\x87\x1b\xb2\xc5.1x] | FAILED | http://localhost:8890/sparql-auth | ERROR 1"
         expected_msg = "<br>".join([msg1, msg2])
         self.assertEqual(received_msg, expected_msg)
+
+    @patch('brainiak.triplestore.SPARQLWrapper.SPARQLWrapper.query', side_effect=MockException())
+    def test_status_default_exception_msg(self, mock_query):
+        received_msg = triplestore.status("USER", "PASSWORD")
+        self.assertTrue(received_msg.endswith("Mocked exception"))
+
+    @patch('brainiak.triplestore.settings', SPARQL_ENDPOINT=None, SPARQL_ENDPOINT_HOST='HOST', SPARQL_ENDPOINT_PORT='PORT')
+    def test_endpoint_url_without_endpoint(self, settings):
+        response = triplestore.endpoint_url()
+        self.assertEqual(response, "HOST:PORT")
+
+    @patch('brainiak.triplestore.settings', SPARQL_ENDPOINT='ENDPOINT', SPARQL_ENDPOINT_HOST='HOST', SPARQL_ENDPOINT_PORT='PORT')
+    def test_endpoint_url_with_endpoint(self, settings):
+        response = triplestore.endpoint_url()
+        self.assertEqual(response, "ENDPOINT")
+
+    @patch('brainiak.triplestore.run_query', side_effect=HTTPError(code=401))
+    def test_query_sparql_with_http_error_401(self, run_query):
+        self.assertRaises(HTTPError, triplestore.query_sparql, "")
+
+    @patch('brainiak.triplestore.run_query', side_effect=HTTPError(code=500))
+    def test_query_sparql_with_http_error_500(self, run_query):
+        self.assertRaises(HTTPError, triplestore.query_sparql, "")
+
+    @patch('brainiak.triplestore.run_query', side_effect=HTTPError(code=500))
+    def test_query_sparql_with_http_error_500(self, run_query):
+        self.assertRaises(HTTPError, triplestore.query_sparql, "")
+
+    @patch('brainiak.triplestore.run_query', return_value=MockResponse())
+    def test_query_sparql_withouterror(self, run_query):
+        response = triplestore.query_sparql("")
+        self.assertEqual(response, {})

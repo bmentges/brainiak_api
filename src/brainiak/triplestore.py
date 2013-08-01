0eac84d4241671074127b4b3206a47dcee52a65e
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2013-08-01 16:19:50 -0300

    Refactor: triplestore.query_sparql stopped using settings.py data, now uses triplestore.ini data

--- src/brainiak/triplestore.py
+++ src/brainiak/triplestore.py
@@ -10,20 +10,19 @@ import ujson as json
 
 from brainiak import settings, log
 from brainiak.greenlet_tornado import greenlet_fetch
-
-
-# def _query_sparql(query, credentials):
-#     # will parse the ini file to endpoint_dict
-#     # will call query_sparql_to_endpoint
-#     pass
-
-
-# def query_sparql_to_endpoint(query, endpoint_dict):
-#     # will send the request to the triplestore, using endpoint_dict
-#     pass
+from brainiak.utils.config_parser import parse_section
 
 
 def query_sparql(query, *args, **kw):
+    # will send the request to the triplestore, using endpoint_dict
+    credentials = kw.get("credentials", {"client_id": "default"})
+    endpoint_dict = parse_section(section=credentials["client_id"])
+    endpoint_dict.pop("app_name")
+    kw["endpoint_dict"] = endpoint_dict
+    return _query_sparql(query, *args, **kw)
+
+
+def _query_sparql(query, *args, **kw):
     """
     Simple interface that given a SPARQL query string returns a string representing a SPARQL results bindings
     in JSON format. For now it only works with Virtuoso, but in futurw we intend to support other databases
@@ -41,50 +40,23 @@ def query_sparql(query, *args, **kw):
     result_dict = json.loads(query_response.body)
     return result_dict
 
-
-SPARQL_RESULTS_FORMAT = {
-    "json": "application/sparql-results+json",
-    "turtle": "text/rdf+n3"
-}
-
-DEFAULT_FORMAT = SPARQL_RESULTS_FORMAT["json"]
-
-URL_ENCODED = "application/x-www-form-urlencoded"
-
-DEFAULT_CONTENT_TYPE = URL_ENCODED
-
-
-def endpoint_url():
-    url = getattr(settings, "SPARQL_ENDPOINT", None)
-    if url is None:
-        host = settings.SPARQL_ENDPOINT_HOST
-        port = settings.SPARQL_ENDPOINT_PORT
-        url = "{0}:{1}".format(host, port)
-    return url
-
 # This is based on virtuoso_connector app, used by App Semantica, so QA2 Virtuoso Analyser works
 format = "POST - %(url)s - %(user_ip)s - %(auth_username)s [tempo: %(time_diff)s] - QUERY - %(query)s"
 
 
 def run_query(query, *args, **kw):
-    method = kw.get("method", "POST")
     params = {
         "query": unicode(query).encode("utf-8"),
-        "format": kw.get("result_format", DEFAULT_FORMAT)
+        "format": "application/sparql-results+json"
     }
-    body = urllib.urlencode(params) if method == "POST" else ""
-    url = endpoint_url()
-    url = url if method == "POST" else url_concat(url, params)
+    body = urllib.urlencode(params)
 
     request_params = {
-        "url": url,
-        "method": method,
-        "headers": {"Content-Type": DEFAULT_CONTENT_TYPE},
-        "body": body,
-        "auth_username": getattr(settings, "SPARQL_ENDPOINT_USER", None),
-        "auth_password": getattr(settings, "SPARQL_ENDPOINT_PASSWORD", None),
-        "auth_mode": getattr(settings, "SPARQL_ENDPOINT_AUTH_MODE", "basic"),
+        "method": "POST",
+        "headers": {"Content-Type": "application/x-www-form-urlencoded"},
+        "body": body
     }
+    request_params = dict(request_params.items(), **kw.get('endpoint_dict', {}))
     request = HTTPRequest(**request_params)
     time_i = time.time()
     response = greenlet_fetch(request)

7320c9bc33d0cc8b2304c3367f80cb6a3918d55a
Diogo Kiss <diogokiss@corp.globo.com>
2013-08-01 09:50:07 -0300

    Corrigindo logica do caso onde nao se tem nem csshX nem cssh.

--- config/modules/ssh.rb
+++ config/modules/ssh.rb
@@ -7,10 +7,10 @@ def ssh_connect(target_role)
         exit 1
     end
 
-    csshbin = %x[which cssh csshX].split("\n").shift
+    csshbin = %x[whereis acssh bcsshX].split("\n").shift
     if roles[target_role].servers.length == 1
       run_local "ssh -l #{user} " + roles[target_role].servers.shift.to_s
-    elsif not csshbin.empty?
+    elsif not csshbin.nil? and not csshbin.empty?
       run_local "#{csshbin} -l #{user} " + roles[target_role].servers.join(' ')
     else
       puts "Atenção: Não foi possível encontrar nenhum dos executáveis csshX ou cssh. O acesso será a apenas um dos servidores da role #{target_role}."

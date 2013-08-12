1f3c7be8b596e1746436c5bc2eb6c1c332f56847
rodsenra <rodsenra@gmail.com>
2013-08-12 11:10:48 -0300

    Adds RequiredParamDict class. Adds new interactive tests cases to help range_search primitive

---
+++ tests/interactive/query_elasticsearch.sh
@@ -0,0 +1,3 @@
+#curl -i -X GET -T query_elasticsearch.json http://esearch.globoi.com/semantica.glb/http%3A%2F%2Fsemantica.globo.com%2Fbase%2FCidade/_search
+curl -i -X GET -T query_elasticsearch.json http://esearch.globoi.com/semantica.glb/_search
+

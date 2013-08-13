40a1f1bd78fac3103cc22acf2ffd340b904a8f6c
Icaro Medeiros <icaro.medeiros@gmail.com>
2013-08-13 19:45:39 -0300

    validating class restriction - issue #9871 @01:00

--- tests/unit/test_range_search.py
+++ tests/unit/test_range_search.py
@@ -1,6 +1,10 @@
 from unittest import TestCase
 
-from brainiak.range_search.range_search import _build_body_query
+from mock import patch
+
+from tornado.web import HTTPError
+
+from brainiak.range_search.range_search import _build_body_query, _validate_class_restriction
 
 
 class RangeSearchTestCase(TestCase):
@@ -20,3 +24,33 @@ class RangeSearchTestCase(TestCase):
 
         response = _build_body_query(params)
         self.assertEqual(expected, response)
+
+    @patch("brainiak.range_search.range_search.filter_values", return_value=["class1", "class2"])
+    def test_validate_classes_restriction(self, mocked_filter_values):
+        expected = ["class1"]
+
+        params = {
+            "restrict_classes": ["class1"]
+        }
+
+        response = _validate_class_restriction(params, None)  # None because filter_values is mocked
+        self.assertEqual(expected, response)
+
+    @patch("brainiak.range_search.range_search.filter_values", return_value=["class1", "class2"])
+    def test_validate_classes_restriction_with_no_restriction_param(self, mocked_filter_values):
+        expected = ["class1", "class2"]
+
+        params = {
+            "restrict_classes": None
+        }
+
+        response = _validate_class_restriction(params, None)  # None because filter_values is mocked
+        self.assertListEqual(sorted(expected), sorted(response))
+
+    @patch("brainiak.range_search.range_search.filter_values", return_value=["class1", "class2"])
+    def test_validate_classes_restriction_raises_error(self, mocked_filter_values):
+        params = {
+            "restrict_classes": ["class1", "class2", "class3"],
+            "predicate": "predicate1"
+        }
+        self.assertRaises(HTTPError, _validate_class_restriction, params, None) # None because filter_values is mocked

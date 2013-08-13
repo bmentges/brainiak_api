69f6997658f95833a33e8de598b1c156a983596b
Icaro Medeiros <icaro.medeiros@gmail.com>
2013-08-13 19:45:39 -0300

    validating class restriction - issue #9871 @01:00

--- src/brainiak/range_search/range_search.py
+++ src/brainiak/range_search/range_search.py
@@ -1,13 +1,21 @@
-from brainiak.utils.sparql import add_language_support
+from tornado.web import HTTPError
+
+from brainiak.utils.sparql import add_language_support, compress_keys_and_values, \
+    filter_values
 from brainiak import triplestore
 
 
 def do_range_search(params):
+    range_result = _get_predicate_ranges(params)
+    range_result = compress_keys_and_values(range_result)
+
+    classes = _validate_class_restriction(params, range_result)
+    graphs = _validate_graph_restriction(params, range_result)
     return None
 
 
 QUERY_PREDICATE_RANGES = """
-SELECT DISTINCT ?range ?label_range {
+SELECT DISTINCT ?range ?range_label ?range_graph {
   {
     <%(predicate)s> rdfs:range ?root_range .
   }
@@ -20,14 +28,15 @@ SELECT DISTINCT ?range ?label_range {
   }
   FILTER (!isBlank(?root_range))
   ?range rdfs:subClassOf ?root_range OPTION(TRANSITIVE, t_min (0)) .
-  ?range rdfs:label ?label_range .
-  %(lang_filter_label_range)s
+  ?range rdfs:label ?range_label .
+  GRAPH ?range_graph { ?range a owl:Class } .
+  %(lang_filter_range_label)s
 }
 """
 
 
 def _build_predicate_ranges_query(query_params):
-    (params, language_tag) = add_language_support(query_params, "label_range")
+    (params, language_tag) = add_language_support(query_params, "range_label")
     return QUERY_PREDICATE_RANGES % params
 
 
@@ -36,9 +45,24 @@ def _get_predicate_ranges(params):
     return triplestore.query_sparql(query, params.triplestore_config)
 
 
+# call search_engine.py
 def _get_search_results(params):
     pass
 
+
+def _validate_class_restriction(params, range_result):
+    classes = set(filter_values(range_result, "range"))
+    if params["restrict_classes"] is not None:
+        classes_not_in_range = list(set(params["restrict_classes"]).difference(classes))
+        if classes_not_in_range:
+            raise HTTPError(400,
+                            "Classes {0} are not in the range of predicate '{1}'".format(classes_not_in_range, params["predicate"]))
+        classes = params["restrict_classes"]
+
+    return list(classes)
+
+def _validate_graph_restriction(params, range_result):
+    pass
 
 def _build_body_query(params):
     patterns = params["pattern"].lower().split()

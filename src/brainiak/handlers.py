5feac96bf0d94b8694fb2436fa262b65cbce06c7
Icaro Medeiros <icaro.medeiros@gmail.com>
2013-08-13 16:31:08 -0300

    predicate ranges query - issue #9871 @01:00

--- src/brainiak/handlers.py
+++ src/brainiak/handlers.py
@@ -21,7 +21,7 @@ from brainiak.instance.edit_instance import edit_instance, instance_exists
 from brainiak.instance.get_instance import get_instance
 from brainiak.log import get_logger
 from brainiak.prefix.get_prefixes import list_prefixes
-from brianiak.range_search.range_search import do_range_search
+from brainiak.range_search.range_search import do_range_search
 from brainiak.root.get_root import list_all_contexts
 from brainiak.root.json_schema import schema as root_schema
 from brainiak.schema import get_class as schema_resource

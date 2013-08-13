618dc5d6432eae602a2f241845f4e04029e1310d
Icaro Medeiros <icaro.medeiros@gmail.com>
2013-08-13 16:31:08 -0300

    predicate ranges query - issue #9871 @01:00

--- src/brainiak/range_search/range_search.py
+++ src/brainiak/range_search/range_search.py
@@ -1,2 +1,35 @@
+from brainiak.utils.sparql import add_language_support
+from brainiak import triplestore
+
 def do_range_search(params):
     pass
+
+
+QUERY_PREDICATE_RANGES = """
+SELECT DISTINCT ?range ?label_range {
+  {
+    <%(predicate)s> rdfs:range ?root_range .
+  }
+  UNION {
+    <%(predicate)s> rdfs:range ?blank .
+    ?blank a owl:Class .
+    ?blank owl:unionOf ?enumeration .
+    OPTIONAL { ?enumeration rdf:rest ?list_node OPTION(TRANSITIVE, t_min (0)) } .
+    OPTIONAL { ?list_node rdf:first ?root_range } .
+  }
+  FILTER (!isBlank(?root_range))
+  ?range rdfs:subClassOf ?root_range OPTION(TRANSITIVE, t_min (0)) .
+  ?range rdfs:label ?label_range .
+  %(lang_filter_label_range)s
+}
+"""
+
+
+def build_predicate_ranges_query(query_params):
+    (params, language_tag) = add_language_support(query_params, "label_range")
+    return QUERY_PREDICATE_RANGES % params
+
+
+def get_predicate_ranges(params):
+    query = build_predicate_ranges_query(params)
+    return triplestore.query_sparql(query, params.triplestore_config)
--- tests/integration/test_range_search.py
+++ tests/integration/test_range_search.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 import json
-from tests.tornado_cases import TornadoAsyncHTTPTestCase
+
 from brainiak.handlers import HTTPError
+from brainiak.range_search.range_search import QUERY_PREDICATE_RANGES
+from brainiak.utils.sparql import filter_values
+
+from tests.tornado_cases import TornadoAsyncHTTPTestCase
+from tests.sparql import QueryTestCase
 
 class TestRangeSearch(TornadoAsyncHTTPTestCase):
 
@@ -13,3 +18,27 @@ class TestRangeSearch(TornadoAsyncHTTPTestCase):
 
     # def test_range_search_without_required_param_predicate(self):
     #     self.assertRaises(HTTPError, self.fetch, '/_range_search?pattern=12')
+
+
+class TestQueryPredicatesRange(QueryTestCase):
+
+    allow_triplestore_connection = True
+    fixtures = ["tests/sample/animalia.n3"]
+    graph_uri = "http://example.onto/"
+
+    maxDiff = None
+
+    def test_query_predicate_superclass_range(self):
+        expected_classes = ["http://example.onto/Place", "http://example.onto/City"]
+        expected_labels = ["Place", "City"]
+
+        query_params = {
+            "predicate": "http://example.onto/birthPlace",
+            "lang_filter_label_range": ""
+        }
+        query_response = self.query(QUERY_PREDICATE_RANGES % query_params)
+        response_classes = filter_values(query_response, "range")
+        response_labels = filter_values(query_response, "label_range")
+
+        self.assertEqual(expected_classes, response_classes)
+        self.assertEqual(expected_labels, response_labels)

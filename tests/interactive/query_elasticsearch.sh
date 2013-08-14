c4466060cc272ca583d7fddcdd3f69eec8051857
Icaro Medeiros <icaro.medeiros@gmail.com>
2013-08-14 11:37:47 -0300

    interactive test in local ES - issue #9871 @01:00

--- tests/interactive/query_elasticsearch.sh
+++ tests/interactive/query_elasticsearch.sh
@@ -1,3 +1,1 @@
-curl -i -X GET -T query_elasticsearch.json http://esearch.globoi.com/semantica.glb/http%3A%2F%2Fsemantica.globo.com%2Fbase%2FCidade/_search
-#curl -i -X GET -T query_elasticsearch.json http://esearch.globoi.com/semantica.glb/_search
-
+curl -X GET -T query_elasticsearch.json http://esearch.globoi.com/semantica.place/http%3A%2F%2Fsemantica.globo.com%2Fplace%2FCity/_search | python -mjson.tool

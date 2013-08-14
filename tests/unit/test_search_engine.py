50ec3ab1a468e41bbf57d4ad655db8df74e3e9ed
Icaro Medeiros <icaro.medeiros@gmail.com>
2013-08-14 14:43:45 -0300

    fixing merge - issue #9871 @00:10

--- tests/unit/test_search_engine.py
+++ tests/unit/test_search_engine.py
@@ -10,23 +10,7 @@ class SearchEngineTestCase(TestCase):
     @patch("brainiak.search_engine.ELASTICSEARCH_ENDPOINT", "http://esearch.dev.globoi.com")
     def test_build_elasticsearch_request_url_all_none(self):
         expected_url = "http://esearch.dev.globoi.com/semantica.*/_search"
-<<<<<<< HEAD
-        response = _build_elasticsearch_request_url(None, None)
-        self.assertEquals(expected_url, response)
-
-    @patch("brainiak.search_engine.ELASTICSEARCH_ENDPOINT", "http://esearch.dev.globoi.com")
-    def test_build_elasticsearch_request_url_indexes_none(self):
-        expected_url = "http://esearch.dev.globoi.com/semantica.*/class_uri_encoded/_search"
-        response = _build_elasticsearch_request_url(None, ["class_uri_encoded"])
-        self.assertEquals(expected_url, response)
-
-    @patch("brainiak.search_engine.ELASTICSEARCH_ENDPOINT", "http://esearch.dev.globoi.com")
-    def test_build_elasticsearch_request_url_types_none(self):
-        expected_url = "http://esearch.dev.globoi.com/semantica.glb/_search"
-        response = _build_elasticsearch_request_url(["semantica.glb"], None)
-=======
         response = _build_elasticsearch_request_url(None)
->>>>>>> 09f45fcae70ca7cd07789d89bc8d947de8cd1f25
         self.assertEquals(expected_url, response)
 
     @patch("brainiak.search_engine.ELASTICSEARCH_ENDPOINT", "http://esearch.dev.globoi.com")

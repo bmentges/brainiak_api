f7db818579bc973bbbc31b8c9fe768fb056acc8c
Icaro Medeiros <icaro.medeiros@gmail.com>
2013-08-14 14:35:49 -0300

    changing test interactive to match our json request to ES - issue #9871 @01:00

--- tests/interactive/query_elasticsearch.sh
+++ tests/interactive/query_elasticsearch.sh
@@ -1,1 +1,1 @@
-curl -X GET -T query_elasticsearch.json http://esearch.globoi.com/semantica.place/http%3A%2F%2Fsemantica.globo.com%2Fplace%2FCity/_search | python -mjson.tool
+curl -X GET -T query_elasticsearch.json http://esearch.dev.globoi.com/semantica.*/_search | python -mjson.tool

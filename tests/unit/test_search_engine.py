f2372f7e0afef12a52ee17193c99d4a22e272fad
Icaro Medeiros <icaro.medeiros@gmail.com>
2013-08-16 17:57:38 -0300

    fixing search engine connection - issue #9781 @03:00

--- tests/unit/test_search_engine.py
+++ tests/unit/test_search_engine.py
@@ -7,13 +7,13 @@ from brainiak.search_engine import _build_elasticsearch_request_url
 
 class SearchEngineTestCase(TestCase):
 
-    @patch("brainiak.search_engine.ELASTICSEARCH_ENDPOINT", "http://esearch.dev.globoi.com")
+    @patch("brainiak.search_engine.ELASTICSEARCH_ENDPOINT", "esearch.dev.globoi.com")
     def test_build_elasticsearch_request_url_all_none(self):
         expected_url = "http://esearch.dev.globoi.com/semantica.*/_search"
         response = _build_elasticsearch_request_url(None)
         self.assertEquals(expected_url, response)
 
-    @patch("brainiak.search_engine.ELASTICSEARCH_ENDPOINT", "http://esearch.dev.globoi.com")
+    @patch("brainiak.search_engine.ELASTICSEARCH_ENDPOINT", "esearch.dev.globoi.com")
     def test_build_elasticsearch_request_url(self):
         expected_url = "http://esearch.dev.globoi.com/semantica.glb/_search"
         response = _build_elasticsearch_request_url(["semantica.glb"])

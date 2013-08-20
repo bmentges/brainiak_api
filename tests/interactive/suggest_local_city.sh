a5a48415b7a48fbecfb1754bc051c082fcbb3173
Icaro Medeiros <icaro.medeiros@gmail.com>
2013-08-20 17:18:51 -0300

    fixing do_item_count - issue #9781 @00:30

--- tests/interactive/suggest_local_city.sh
+++ tests/interactive/suggest_local_city.sh
@@ -1,1 +1,1 @@
-curl -X POST http://localhost:5100/_range_search -T suggest_local_city.json
+curl -X POST http://localhost:5100/_range_search?do_item_count=1 -T suggest_local_city.json | python -m json.tool

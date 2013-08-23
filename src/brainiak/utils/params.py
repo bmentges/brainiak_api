fc7f0d3f3dc459d4af7a2eb0b7bddef811ee95c6
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2013-08-23 11:22:51 -0300

    Fix class_uri param behavior at utils.params

--- src/brainiak/utils/params.py
+++ src/brainiak/utils/params.py
@@ -6,7 +6,7 @@ from urlparse import unquote, parse_qs
 from tornado.web import HTTPError
 
 from brainiak import settings
-from brainiak.prefixes import safe_slug_to_prefix
+from brainiak.prefixes import expand_uri, safe_slug_to_prefix
 from brainiak.utils.sparql import PATTERN_O, PATTERN_P
 from brainiak.utils.config_parser import ConfigParserNoSectionError, parse_section
 
@@ -176,8 +176,11 @@ class ParamDict(dict):
         Changes in *_prefix should reflect in *_uri.
         Changes in expand_uri should reflect in expand_uri_values and expand_uri_keys.
         """
-        if key in ('graph_uri', 'class_uri'):
+        if key  == 'graph_uri':
             dict.__setitem__(self, key, safe_slug_to_prefix(value))
+
+        elif key == 'class_uri':
+            dict.__setitem__(self, key, expand_uri(value))
 
         elif key == "context_name":
             dict.__setitem__(self, key, value)

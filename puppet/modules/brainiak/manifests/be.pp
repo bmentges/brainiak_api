bd2589b5e1b656f11d710fb01819cd83343a5cf5
Diogo Kiss <diogokiss@corp.globo.com>
2013-09-03 10:25:21 -0300

    Ajustando setup do GUnicorn.

--- puppet/modules/brainiak/manifests/be.pp
+++ puppet/modules/brainiak/manifests/be.pp
@@ -3,20 +3,24 @@
 # Cria o ambiente API para o Brainiak de Semântica.
 # É uma API REST que serve como camada de abstração para acesso ao Virtuoso.
 #
-# Author: Jefferson Braga <jefferson@corp.globo.com>, Diogo Kiss <diogokiss@corp.globo.com> e Marcelo Monteiro <m.monteiro@corp.globo.com>.
+# == Authors
 #
-# == Parameters:
+#   Jefferson Braga <jefferson@corp.globo.com>
+#   Diogo Kiss <diogokiss@corp.globo.com>
+#   Marcelo Monteiro <m.monteiro@corp.globo.com>.
+#
+# == Parameters
 #
 #   Nenhum parametro é necessário para esta classe.
 #
-# == Actions:
+# == Actions
 #
 #   Implementa toda a configuração de ambiente necessária ao projeto.
 #   - Instalar RPMs.
 #   - Criar usuarios, diretorios, links, initscripts e aliases.
 #   - Montar filer.
 #
-# == Sample Usage:
+# == Sample Usage
 #
 #   include brainiak::be
 #
@@ -47,7 +51,33 @@ class brainiak::be inherits brainiak::params {
         require             => Package['python27-virtualenv_generic_globo'],
     }
 
-    # TODO: GUnicor
+    # TODO: GUnicorn  
+    infra::gunicorn { "GUnicorn - Brainiak":
+        projeto             => $projeto,
+        instancia           => 'be',
+        dir                 => "${projeto_home_dir}/gunicorn-be",
+        projeto_usuario     => $usuario,
+        projeto_grupo       => $grupo,
+        instancia_usuario   => $usuario,
+        instancia_grupo     => $grupo,
+        app_dir             => "${projeto_deploybe_dir}/app/current",
+        python_prefix       => $virtualenv_dir,
+        log_dest_dir        => $projeto_logsunix_dir,
+        log_keep            => $gunicorn_log_keep,
+        gunicorn_bin        => 'gunicorn',
+        gunicorn_processes  => $gunicorn_num_processes,
+        gunicorn_loglevel   => $gunicorn_loglevel,
+        gunicorn_debug      => $gunicorn_debug,
+        gunicorn_cmd_parameters => '-k tornado brainiak.server:application',
+        settings_file       => 'settings',
+        dbpasswd            => true,
+        use_mount_virtual   => $projeto,
+        gunicorn_dbpasswd_conf => $dbpasswd_conf_file,
+        sudoers             => $api_semantica::defs::sudoers,
+        autostart           => true,
+        require             => Virtualenv[$python_virtualenv_dir]
+    }
+    
     # TODO: Nginx
     # TODO: Checar expurgo e rotacionamento de logs
 

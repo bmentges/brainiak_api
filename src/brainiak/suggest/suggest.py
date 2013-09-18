a7fc3b4cda070c3d51139465654a1f6fb2d1c365
Icaro Medeiros <icaro.medeiros@gmail.com>
2013-09-18 15:56:40 -0300

    refactoring to differentiate query_params, search_params, and response_params in do_range_search to /_suggest resource #10475 @02:00

--- src/brainiak/suggest/suggest.py
+++ src/brainiak/suggest/suggest.py
@@ -10,30 +10,33 @@ SUGGEST_OPTIONAL_PARAMS = ('search_fields', 'search_classes', 'search_graphs')
 RDFS_LABEL = "http://www.w3.org/2000/01/rdf-schema#label"
 
 
-def do_range_search(params):
-    range_result = _get_predicate_ranges(params)
-    if is_result_empty(range_result):
-        raise HTTPError(400, "Either the predicate {0} does not exists or it does not have any rdfs:range defined in the triplestore".format(params['predicate']))
+def do_range_search(query_params, suggest_params):
+    search_params = suggest_params["search"]
+    response_params = suggest_params.get("response", {})
 
-    classes = _validate_class_restriction(params, range_result)
-    graphs = _validate_graph_restriction(params, range_result)
+    range_result = _get_predicate_ranges(query_params, search_params)
+    if is_result_empty(range_result):
+        raise HTTPError(400, "Either the predicate {0} does not exists or it does not have any rdfs:range defined in the triplestore".format(search_params["target"]))
+
+    classes = _validate_class_restriction(query_params, range_result)
+    graphs = _validate_graph_restriction(query_params, range_result)
     indexes = [_graph_uri_to_index_name(graph) for graph in graphs]
 
     compressed_result = compress_keys_and_values(range_result)
     class_label_dict = _build_class_label_dict(compressed_result)
 
     title_fields = [RDFS_LABEL]
-    title_fields += _get_subproperties(params, RDFS_LABEL)
-    search_fields = list(set(_get_search_fields(params) + title_fields))
+    title_fields += _get_subproperties(query_params, RDFS_LABEL)
+    search_fields = list(set(_get_search_fields(search_params) + title_fields))
 
-    request_body = _build_body_query(params, classes, search_fields)
+    request_body = _build_body_query(query_params, search_params, response_params, classes, search_fields)
     elasticsearch_result = run_search(request_body, indexes=indexes)
     items, item_count = _build_items(elasticsearch_result, class_label_dict, title_fields)
 
     if not items:
         return None
     else:
-        return build_json(items, item_count, params)
+        return build_json(items, item_count, query_params)
 
 
 def build_json(items_list, item_count, query_params):
@@ -53,10 +56,10 @@ def build_json(items_list, item_count, query_params):
 QUERY_PREDICATE_RANGES = """
 SELECT DISTINCT ?range ?range_label ?range_graph {
   {
-    <%(predicate)s> rdfs:range ?root_range .
+    <%(target)s> rdfs:range ?root_range .
   }
   UNION {
-    <%(predicate)s> rdfs:range ?blank .
+    <%(target)s> rdfs:range ?blank .
     ?blank a owl:Class .
     ?blank owl:unionOf ?enumeration .
     OPTIONAL { ?enumeration rdf:rest ?list_node OPTION(TRANSITIVE, t_min (0)) } .
@@ -71,14 +74,15 @@ SELECT DISTINCT ?range ?range_label ?range_graph {
 """
 
 
-def _build_predicate_ranges_query(query_params):
+def _build_predicate_ranges_query(query_params, search_params):
     (params, language_tag) = add_language_support(query_params, "range_label")
+    params.update(search_params)
     return QUERY_PREDICATE_RANGES % params
 
 
-def _get_predicate_ranges(params):
-    query = _build_predicate_ranges_query(params)
-    return triplestore.query_sparql(query, params.triplestore_config)
+def _get_predicate_ranges(query_params, search_params):
+    query = _build_predicate_ranges_query(query_params, search_params)
+    return triplestore.query_sparql(query, query_params.triplestore_config)
 
 
 QUERY_SUBPROPERTIES = """
@@ -95,36 +99,36 @@ def _get_subproperties(params, super_property):
     return filter_values(result, "property")
 
 
-def _get_search_fields(params):
-    search_fields_in_params = params.get("search_fields", [])
-    search_fields = set(search_fields_in_params)
-    for field in search_fields_in_params:
-        sub_properties = _get_subproperties(params, field)
+def _get_search_fields(search_params):
+    search_fields_in_search_params = search_params.get("fields", [])
+    search_fields = set(search_fields_in_search_params)
+    for field in search_fields_in_search_params:
+        sub_properties = _get_subproperties(search_params, field)
         search_fields.update(sub_properties)
 
     return list(search_fields)
 
 
-def _validate_class_restriction(params, range_result):
+def _validate_class_restriction(search_params, range_result):
     classes = set(filter_values(range_result, "range"))
-    if "search_classes" in params:
-        classes_not_in_range = list(set(params["search_classes"]).difference(classes))
+    if "classes" in search_params:
+        classes_not_in_range = list(set(search_params["classes"]).difference(classes))
         if classes_not_in_range:
             raise HTTPError(400,
-                            "Classes {0} are not in the range of predicate '{1}'".format(classes_not_in_range, params["predicate"]))
-        classes = params["search_classes"]
+                            "Classes {0} are not in the range of predicate '{1}'".format(classes_not_in_range, search_params["target"]))
+        classes = search_params["classes"]
 
     return list(classes)
 
 
-def _validate_graph_restriction(params, range_result):
+def _validate_graph_restriction(search_params, range_result):
     graphs = set(filter_values(range_result, "range_graph"))
-    if "search_graphs" in params:
-        graphs_set = set(params["search_graphs"])
+    if "graphs" in search_params:
+        graphs_set = set(search_params["graphs"])
         graphs_not_in_range = list(graphs_set.difference(graphs))
         if graphs_not_in_range:
             raise HTTPError(400,
-                            "Classes in the range of predicate '{0}' are not in graphs {1}".format(params["predicate"], graphs_not_in_range))
+                            "Classes in the range of predicate '{0}' are not in graphs {1}".format(search_params["target"], graphs_not_in_range))
         graphs = graphs_set
 
     graphs = graphs.difference(set(settings.GRAPHS_WITHOUT_INSTANCES))
@@ -132,17 +136,17 @@ def _validate_graph_restriction(params, range_result):
     if not graphs:
         raise HTTPError(400,
                         "Classes in the range of predicate '{0}' are in graphs without instances, such as: {1}".format(
-                            params["predicate"], settings.GRAPHS_WITHOUT_INSTANCES))
+                            search_params["target"], settings.GRAPHS_WITHOUT_INSTANCES))
     return list(graphs)
 
 
-def _build_body_query(params, classes, search_fields):
-    patterns = params["pattern"].lower().split()
+def _build_body_query(query_params, search_params, response_params, classes, search_fields):
+    patterns = search_params["pattern"].lower().split()
     query_string = " AND ".join(patterns) + "*"
-    return_fields = search_fields  # TODO return_fields in params
+    return_fields = search_fields  # TODO fields in response_params
     body = {
-        "from": int(calculate_offset(params)),
-        "size": int(params.get("per_page", settings.DEFAULT_PER_PAGE)),
+        "from": int(calculate_offset(query_params)),
+        "size": int(query_params.get("per_page", settings.DEFAULT_PER_PAGE)),
         "fields": return_fields,
         "query": {
             "query_string": {
@@ -194,7 +198,7 @@ def _build_items(result, class_label_dict, title_fields):
                 "@type": item["_type"],
                 "type_title": class_label_dict[item["_type"]]
             }
-            # return other fields only when return_fields is specified
+            # return other fields only when fields in response_params is specified
             # item_dict.update(item["fields"])
             items.append(item_dict)
 

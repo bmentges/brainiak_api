6722cf2da7423806c86fc229ff558686e0f09942
rodsenra <rodsenra@gmail.com>
2013-09-19 15:15:37 -0300

    Updating docs

--- tests/interactive/suggest_local_city.sh
+++ tests/interactive/suggest_local_city.sh
@@ -1,1 +1,2 @@
-curl -X POST http://localhost:5100/_suggest?do_item_count=1 -T suggest_local_city.json  | python -m json.tool
+curl -i -X POST http://localhost:5100/_suggest?do_item_count=1 -T suggest_local_city.json  
+#| python -m json.tool

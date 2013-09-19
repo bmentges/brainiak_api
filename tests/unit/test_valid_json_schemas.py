e2aa369302edae14d7423056bccbc70a62af943d
Icaro Medeiros <icaro.medeiros@gmail.com>
2013-09-19 12:10:21 -0300

    json schema for suggest response - issue #10475 @00:30

--- tests/unit/test_valid_json_schemas.py
+++ tests/unit/test_valid_json_schemas.py
@@ -5,6 +5,7 @@ from jsonschema import Draft3Validator, SchemaError
 from brainiak.root import json_schema as root_json_schema
 from brainiak.context import json_schema as ctx_json_schema
 from brainiak.collection import json_schema as collection_json_schema
+from brainiak.suggest import json_schema as suggest_json_schema
 
 
 class ResultHandlerTestCase(unittest.TestCase):
@@ -26,4 +27,8 @@ class ResultHandlerTestCase(unittest.TestCase):
 
     def test_valid_json_schema_for_collection(self):
         schema = collection_json_schema.schema('glb', 'Materia', 'http://semantica.globo.com/base/')
+        self.validate_draft3(schema)
+
+    def test_valid_suggest_json_schema(self):
+        schema = suggest_json_schema.schema()
         self.validate_draft3(schema)

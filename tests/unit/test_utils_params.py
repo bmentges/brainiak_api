dc967027d16254d0fdcf1a8da482e824b2ec5eef
Icaro Medeiros <icaro.medeiros@gmail.com>
2013-10-11 15:37:40 -0300

    fixing tests and messages, differentiating between lists with empty items and 404 errors - issue #10500 @03:00

--- tests/unit/test_utils_params.py
+++ tests/unit/test_utils_params.py
@@ -4,8 +4,7 @@ from brainiak import settings
 from brainiak.utils import params
 from brainiak.prefixes import ROOT_CONTEXT, SHORTEN
 from brainiak.settings import URI_PREFIX
-from brainiak.utils.params import ParamDict, InvalidParam, DefaultParamsDict, LIST_PARAMS, INSTANCE_PARAMS, CACHE_PARAMS, PAGING_PARAMS, RequiredParamsDict, optionals, RequiredParamMissing
-from brainiak.utils.resources import valid_pagination
+from brainiak.utils.params import ParamDict, InvalidParam, DefaultParamsDict, LIST_PARAMS, INSTANCE_PARAMS, CACHE_PARAMS, PAGING_PARAMS, RequiredParamsDict, optionals
 from tests.mocks import MockHandler
 
 
@@ -254,12 +253,6 @@ class ParamsTestCase(TestCase):
         handler = MockHandler()
         params = ParamDict(handler, do_item_count='1')
         self.assertEqual(params["do_item_count"], '1')
-
-    def test_pagination_validation(self):
-        self.assertTrue(valid_pagination(total=1, page=0, per_page=10))
-        self.assertTrue(valid_pagination(total=1, page=0, per_page=1))
-        self.assertFalse(valid_pagination(total=10, page=1, per_page=10))
-        self.assertFalse(valid_pagination(total=1, page=1, per_page=1))
 
 
 class ExpandUriTestCase(TestCase):

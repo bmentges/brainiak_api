f14163233cb3a09a07c88cc8c91d2722ca0f1236
rodsenra <rodsenra@gmail.com>
2013-10-16 18:55:45 -0300

    Fix conflict issues after merge

--- src/brainiak/handlers.py
+++ src/brainiak/handlers.py
@@ -22,7 +22,7 @@ from brainiak.instance.delete_instance import delete_instance
 from brainiak.instance.edit_instance import edit_instance, instance_exists
 from brainiak.instance.get_instance import get_instance
 from brainiak.log import get_logger
-from brainiak.prefixes import normalize_all_uris_recursively, list_prefixes
+from brainiak.prefixes import normalize_all_uris_recursively, list_prefixes, SHORTEN
 from brainiak.schema.get_class import SchemaNotFound
 from brainiak.suggest.suggest import do_suggest, SUGGEST_PARAM_SCHEMA
 from brainiak.suggest.json_schema import schema as suggest_schema

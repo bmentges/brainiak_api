834a297dc4cfab4d7d7ef7d8fd298b6a18fa670b
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2013-10-21 11:08:34 -0200

    Fix issue with GET instance by instance_uri, avoiding duplication of properties / values

---
+++ src/brainiak/instance/common.py
@@ -0,0 +1,34 @@
+from tornado.web import HTTPError
+from brainiak import triplestore
+
+QUERY_GET_CLASS_AND_GRAPH = u"""
+SELECT DISTINCT ?class_uri ?graph_uri {
+    GRAPH ?graph_uri { <%(instance_uri)s> a ?class_uri . }
+}
+"""
+
+
+def get_class_and_graph(query_params):
+    query = QUERY_GET_CLASS_AND_GRAPH % query_params
+    return triplestore.query_sparql(query, query_params.triplestore_config)
+
+
+def must_retrieve_graph_and_class_uri(query_params):
+    try:
+        values = [query_params["class_name"], query_params["instance_id"], query_params["graph_uri"]]
+        query_params["instance_uri"]
+    except KeyError as ex:
+        raise HTTPError(404, log_message=u"Parameter <{0:s}> is missing in order to update instance.".format(ex))
+    return all([value == u"_" for value in values])
+
+
+def extract_class_uri(bindings):
+    class_uri = bindings[0]['class_uri']['value']
+    [item.pop('class_uri') for item in bindings]
+    return class_uri
+
+
+def extract_graph_uri(bindings):
+    graph_uri = bindings[0]['graph_uri']['value']
+    [item.pop('graph_uri') for item in bindings]
+    return graph_uri

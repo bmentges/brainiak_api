4335196f02c3e2532eaad45f9dffaa3de70011f2
Icaro Medeiros <icaro.medeiros@gmail.com>
2013-10-23 10:47:29 -0200

    logging when not mapped type is passed to schema

--- tests/unit/test_items_from_range.py
+++ tests/unit/test_items_from_range.py
@@ -1,31 +1,40 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+
+from mock import patch
+
 from brainiak.schema.get_class import items_from_range
 
 
 class ItemsFromRangeTestCase(unittest.TestCase):
 
-    def test_items_from_range_date(self):
+    @patch("brainiak.schema.get_class.logger")
+    def test_items_from_range_date(self, log):
         self.assertEqual(items_from_range("http://www.w3.org/2001/XMLSchema#date"),
                           {"type": "string", "format": "date", "datatype": "http://www.w3.org/2001/XMLSchema#date"})
 
-    def test_items_from_range_integer(self):
+    @patch("brainiak.schema.get_class.logger")
+    def test_items_from_range_integer(self, log):
         self.assertEqual(items_from_range("http://www.w3.org/2001/XMLSchema#integer"),
                           {'datatype': "http://www.w3.org/2001/XMLSchema#integer", 'type': 'integer'})
 
-    def test_items_from_range_float(self):
+    @patch("brainiak.schema.get_class.logger")
+    def test_items_from_range_float(self, log):
         self.assertEqual(items_from_range("http://www.w3.org/2001/XMLSchema#float"),
                           {'datatype': "http://www.w3.org/2001/XMLSchema#float", 'type': 'number'})
 
-    def test_items_from_range_string(self):
+    @patch("brainiak.schema.get_class.logger")
+    def test_items_from_range_string(self, log):
         self.assertEqual(items_from_range("http://www.w3.org/2001/XMLSchema#string"),
                           {'datatype': "http://www.w3.org/2001/XMLSchema#string", 'type': 'string'})
 
-    def test_items_from_range_string_any_uri(self):
+    @patch("brainiak.schema.get_class.logger")
+    def test_items_from_range_string_any_uri(self, log):
         self.assertEqual(items_from_range("http://www.w3.org/2001/XMLSchema#anyURI"),
                           {'datatype': "http://www.w3.org/2001/XMLSchema#anyURI", 'type': 'string'})
 
-    def test_items_from_range_unmapped(self):
+    @patch("brainiak.schema.get_class.logger")
+    def test_items_from_range_unmapped(self, log):
         self.assertEqual(items_from_range("http://some/strange/type/uri"),
                           {'datatype': 'http://some/strange/type/uri', 'type': 'string'})

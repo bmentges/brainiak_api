9c44d0ee7fced99b04f10a916a47ff8c66dae8e5
Icaro Medeiros <icaro.medeiros@gmail.com>
2013-11-08 17:02:48 -0200

    refactored parameter validation for pattern - issue #10760 @00:40

--- tests/integration/test_search.py
+++ tests/integration/test_search.py
@@ -45,5 +45,18 @@ class SearchIntegrationTestCase(TornadoAsyncHTTPTestCase):
         del response_dict["_base_url"] # This varies from request to request locally because tornado bind a random port
         self.assertEqual(response_dict, expected_answer_dict)
 
-    def test_search_not_found(self):
-        pass
+    @patch("brainiak.search.search.uri_to_slug", return_value="example.onto")
+    def test_search_not_found(self, mock_uri_to_slug):
+        expected_items = {}
+        response = self.fetch('/_search?pattern=non_existent_pattern' +
+           '&graph_uri=http://example.onto/' +
+           '&class_uri=http://example.onto/City')
+        response_dict = json.loads(response.body)
+        response_items = response_dict["items"]
+        self.assertEqual(expected_items, response_items)
+
+    def test_search_with_missing_parameters(self):
+        response = self.fetch('/_search')
+        self.assertEqual(response.code, 400)
+        error_message = response.body
+        self.assertEqual(error_message, '{"errors": ["HTTP error: 400\\nRequired parameter (pattern) was not given."]}')

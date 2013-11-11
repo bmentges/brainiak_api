d4e6267f3210718d7a5d87a83688e0da34fcfdbb
rodsenra <rodsenra@gmail.com>
2013-11-11 15:14:14 -0200

    Refactoring json-schema for search. When setting graph_uri -> automatically define context_name. When setting class_uri automatically set class_name .

--- src/brainiak/search/json_schema.py
+++ src/brainiak/search/json_schema.py
@@ -14,6 +14,84 @@ SEARCH_PARAM_SCHEMA = {
 
 
 def schema():
-    base = {}
-    merge_schemas(base, pagination_schema('/', method="POST"))
+    schema_ref = u"/{{_context_name}}/{{_class_name}}/_schema?class_prefix={{_class_prefix}}"
+    href = u"/{{_context_name}}/{{_class_name}}/?class_prefix={{_class_prefix}}"
+    link = u"/{{_context_name}}/{{_class_name}}/{{resource_id}}?class_prefix={{_class_prefix}}&instance_prefix={{instance_prefix}}"
+
+    base = {
+        "$schema": "http://json-schema.org/draft-04/schema#",
+        "title": "Collection Schema that lists instances",
+        "type": "object",
+        "required": ["items", "_class_prefix", "@id"],
+        "properties": {
+            "pattern": {"type": "string"},  # used in _search service responses
+            "_graph_uri": {"type": "string"},  # used in _search service responses
+            "_context_name": {"type": "string"},  # used in _search service responses
+            "_class_name": {"type": "string"},  # used in _search service responses
+            "_class_prefix": {"type": "string"},  # used in _search service responses
+            "_class_uri": {"type": "string"},  # used in _search service responses
+            "do_item_count": {"type": "integer"},
+            "item_count": {"type": "integer"},
+            "@id": {"type": "string", "format": "uri"},
+            "items": {
+                "type": "array",
+                "items": {
+                    "type": "object",
+                    "required": ["title", "@id", "resource_id", "instance_prefix"],
+                    "properties": {
+                        "title": {"type": "string"},
+                        "@id": {"type": "string"},
+                        "resource_id": {"type": "string"},
+                        "instance_prefix": {"type": "string", "format": "uri"},
+                    },
+                    "links": [
+                        {
+                            "href": link,
+                            "method": "GET",
+                            "rel": "item"
+                        },
+                        {
+                            "href": link,
+                            "method": "GET",
+                            "rel": "instance"
+                        }
+                    ]
+                }
+            },
+        },
+        "links": [
+            {
+                "href": "{+_base_url}",
+                "method": "GET",
+                "rel": "self"
+            },
+            {
+                "href": "{+_schema_url}",
+                "method": "GET",
+                "rel": "class"
+            },
+            {
+                "href": u"/{{_context_name}}",
+                "method": "GET",
+                "rel": "context"
+            },
+            {
+                "href": href,
+                "method": "POST",
+                "rel": "add",
+                "schema": {"$ref": schema_ref}
+            },
+            {
+                "href": "/_search?graph_uri={{_graph_uri}}&class_uri={{_class_uri}}&pattern={{pattern}}",
+                "method": "GET",
+                "rel": "search",
+                "schema": SEARCH_PARAM_SCHEMA
+            }
+        ]
+    }
+
+    base_pagination_url = u'/{{_context_name}}/{{_class_name}}'
+    extra_url_params = '&class_prefix={_class_prefix}'
+    pagination_dict = pagination_schema(base_pagination_url, extra_url_params)
+    merge_schemas(base, pagination_dict)
     return base

26b15b1ca840b9bb625d3da27eb9dcd30e2e174e
Icaro Medeiros <icaro.medeiros@gmail.com>
2013-11-27 15:32:08 -0200

    Added environment variable to get semantica password in cmaas interface. Added test that clicks in + in dashboard and opens add instance form

--- tests/acceptance/cma/test_login.py
+++ tests/acceptance/cma/test_login.py
@@ -1,24 +1,15 @@
 from time import sleep
-from unittest import TestCase
 
-from splinter import Browser
-
-from helpers import do_login
+from helpers import do_login, SplinterTestCase
 
 
-class CMALoginTestCase(TestCase):
-
-    def setUp(self):
-        self.browser = Browser()
-
-    def tearDown(self):
-        self.browser.quit()
+class CMALoginTestCase(SplinterTestCase):
 
     def test_wrong_login(self):
-        do_login(self.browser, "user", "password")
-        self.assertTrue(self.browser.is_element_present_by_css("div .error"))
+        do_login(self.browser, user="user", password="password")
+        self.assertTrue(self.browser.is_element_present_by_css("div.error"))
 
     def test_successful_login_goes_to_dashboard(self):
-        do_login(self.browser, "icaro.medeiros", "pass") # please change using your password while we dont have a test user
+        do_login(self.browser)
         sleep(3)
-        self.assertTrue(self.browser.is_element_present_by_css("div .dashboard"))
+        self.assertTrue(self.browser.is_element_present_by_xpath("//h2[.='Eureka - Assunto Eureka']"))

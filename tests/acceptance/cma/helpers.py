20851bd427c287c709d05e580f31bccb7c5949b8
Icaro Medeiros <icaro.medeiros@gmail.com>
2013-11-27 15:32:08 -0200

    Added environment variable to get semantica password in cmaas interface. Added test that clicks in + in dashboard and opens add instance form

--- tests/acceptance/cma/helpers.py
+++ tests/acceptance/cma/helpers.py
@@ -1,6 +1,16 @@
+import os
 from time import sleep
+from unittest import TestCase
 
-def do_login(browser, user, password):
+from splinter import Browser
+
+ADMIN_USER = "semantica"
+ADMIN_PASSWORD = os.getenv("SEMANTICA_USER_PASSWORD")
+
+assert ADMIN_PASSWORD is not None, "Define environment variable SEMANTICA_USER_PASSWORD: export SEMANTICA_USER_PASSWORD=pass"
+
+
+def do_login(browser, user=ADMIN_USER, password=ADMIN_PASSWORD):
     browser.visit('http://admin.backstage.dev.globoi.com')
     sleep(2)
 
@@ -9,3 +19,14 @@ def do_login(browser, user, password):
 
     button = browser.find_by_name('button')
     button.click()
+
+# login_and_enter_dashboard()
+
+
+class SplinterTestCase(TestCase):
+
+    def setUp(self):
+        self.browser = Browser()
+
+    def tearDown(self):
+        self.browser.quit()

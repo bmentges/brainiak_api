1dd5acf7efc64af24f201b3e6af2f1f1e384bd57
Diogo Kiss <diogokiss@corp.globo.com>
2014-01-08 16:02:28 -0200

    Added new server in QA environment.

--- config/deploy/qa.rb
+++ config/deploy/qa.rb
@@ -1,4 +1,4 @@
-role :be,                         'cittavld895.globoi.com'
+role :be,                         'cittavld895.globoi.com', 'cittavld896.globoi.com'
 role :filer,                      'filer.qa.globoi.com'
 
 # Hosts

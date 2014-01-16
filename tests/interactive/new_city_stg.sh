be391ad41da71cd1fe4e35e3da15d992a6f18c31
rodsenra <rodsenra@gmail.com>
2014-01-16 11:36:49 -0200

    Fix version and interactive tests

--- tests/interactive/new_city_stg.sh
+++ tests/interactive/new_city_stg.sh
@@ -1,1 +1,1 @@
-curl -i -X POST -T "new_city.json" --proxy1.0 proxy.staging.globoi.com:3128 http://api.semantica.globoi.com/place/City
+curl -i -X POST -T "new_city.json" --proxy1.0 proxy.staging.globoi.com:3128 http://brainiak.semantica.globoi.com/place/City

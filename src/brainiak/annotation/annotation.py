bc6d986ba518fa1948e1a41794e1905cbfa1a6e2
Romulo Jales <romulo.jales@corp.globo.com>
2014-03-10 18:42:49 -0300

    from annotation to uri

--- src/brainiak/annotation/annotation.py
+++ src/brainiak/annotation/annotation.py
@@ -18,11 +18,11 @@ SELECT ?uri ?permalink FROM <{graph_uri}> WHERE {{
      base:status_de_publicacao "P";
      base:data_da_primeira_publicacao ?data_da_primeira_publicacao;
      base:permalink ?permalink;
-     ?p <{annotation}>.
+     ?p <{uri}>.
 }}ORDER BY {sort_order}(?data_da_primeira_publicacao)
 limit {per_page}"""
 
-ANNOTATIONS_PARAMS = LIST_PARAMS + RequiredParamsDict(annotation="")
+ANNOTATIONS_PARAMS = LIST_PARAMS + RequiredParamsDict(uri="")
 
 
 def query_annotation(query_params):

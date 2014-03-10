02c20e7c0f86171fe153d9829497d73c2f70ee9d
Romulo Jales <romulo.jales@corp.globo.com>
2014-03-10 18:49:09 -0300

    wew, now the name is object_uri

--- src/brainiak/annotation/annotation.py
+++ src/brainiak/annotation/annotation.py
@@ -18,11 +18,10 @@ SELECT ?uri ?permalink FROM <{graph_uri}> WHERE {{
      base:status_de_publicacao "P";
      base:data_da_primeira_publicacao ?data_da_primeira_publicacao;
      base:permalink ?permalink;
-     ?p <{uri}>.
+     ?p <{object_uri}>.
 }}ORDER BY {sort_order}(?data_da_primeira_publicacao)
 limit {per_page}"""
 
-ANNOTATIONS_PARAMS = LIST_PARAMS + RequiredParamsDict(uri="")
 
 
 def query_annotation(query_params):

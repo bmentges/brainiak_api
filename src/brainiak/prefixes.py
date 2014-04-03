8f8e017b608715989a82dbdecb52e53941e9375e
Icaro Medeiros <icaro.medeiros@gmail.com>
2014-04-03 17:07:29 -0300

    adding techtudo prefixes

--- src/brainiak/prefixes.py
+++ src/brainiak/prefixes.py
@@ -58,6 +58,8 @@ LEGACY_PREFIXES = {
     "g1": "http://semantica.globo.com/G1/",
     "tvg": "http://semantica.globo.com/tvg/",
     "eureka": "http://semantica.globo.com/eureka/",
+    "tecnologia": "http://semantica.globo.com/tecnologia/",
+    "techtudo": "http://semantica.globo.com/techtudo/"
 }
 
 _MAP_SLUG_TO_PREFIX.update(STANDARD_PREFIXES)

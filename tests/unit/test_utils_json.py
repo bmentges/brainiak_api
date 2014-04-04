2ee604d7a2bafc35650c7c4e1bccee5b67f8102e
Icaro Medeiros <icaro.medeiros@gmail.com>
2014-04-04 18:48:07 -0300

    coverage for utils/json

--- tests/unit/test_utils_json.py
+++ tests/unit/test_utils_json.py
@@ -4,7 +4,8 @@ from unittest import TestCase
 from jsonschema import ValidationError
 from tornado.web import HTTPError
 
-from brainiak.utils.json import validate_json_schema
+from brainiak.utils.json import validate_json_schema,\
+    get_json_request_as_dict
 
 
 class JSONTestCase(TestCase):
@@ -16,6 +17,17 @@ class JSONTestCase(TestCase):
                 "items": {"type": "array"}
             }
     }
+
+    def test_get_valid_json(self):
+        valid_json_string = '{"items": []}'
+        response = get_json_request_as_dict(valid_json_string)
+        self.assertEqual(response["items"], [])
+
+    def test_get_invalid_json(self):
+        invalid_json_string = '{[][]}'
+        self.assertRaises(HTTPError,
+            get_json_request_as_dict,
+            invalid_json_string)
 
     @patch("brainiak.utils.json.validate")
     def test_valid_json_schema(self, mocked_validate):

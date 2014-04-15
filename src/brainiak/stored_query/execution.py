7111c401e7c5f56eddda944fa9cd689f961026dc
Icaro Medeiros <icaro.medeiros@gmail.com>
2014-04-15 10:30:14 -0300

    missed included renamed files

---
+++ src/brainiak/stored_query/execution.py
@@ -0,0 +1,46 @@
+import copy
+
+from tornado.web import HTTPError
+
+from brainiak import log
+from brainiak.triplestore import query_sparql
+from brainiak.utils.i18n import _
+from brainiak.utils.sparql import compress_keys_and_values
+
+
+QUERY_EXECUTION_LOG_FORMAT = "Stored Query [{query_id}] - app {app_name} - {url} - {query}"
+
+NO_RESULTS_MESSAGE_FORMAT = "The query returned no results. SPARQL endpoint [{0}]\n  Query: {1}"
+
+
+def execute_query(query_id, stored_query, querystring_params):
+    query = get_query(stored_query, querystring_params)
+
+    # TODO extract_method?
+    request_dict = copy.copy(querystring_params.triplestore_config)
+    request_dict.update({"query_id": query_id})
+    request_dict.update({"query": query})
+    log.logger.info(QUERY_EXECUTION_LOG_FORMAT.format(**request_dict))
+
+    result_dict = query_sparql(query,
+                               querystring_params.triplestore_config)
+    items = compress_keys_and_values(result_dict)
+    if not items:
+        message = NO_RESULTS_MESSAGE_FORMAT.format(querystring_params.triplestore_config["url"], query)
+        return {
+            "items": [],
+            # TODO explain in which instance of Virtuoso the query was executed?
+            "warning": message}
+    return {"items": items}
+
+
+def get_query(stored_query, querystring_params):
+    # template existence is validated in stored query creation/modification
+    template = stored_query["sparql_template"]
+    try:
+        # .arguments is a dict with decoded querystring params
+        query = template % querystring_params.arguments
+        return query
+    except KeyError as key_error:
+        missing_key_message = _("Missing key '{0}' in querystring").format(key_error.args[0])
+        raise HTTPError(400, log_message=missing_key_message)
---
+++ tests/unit/test_stored_query_execution.py
@@ -0,0 +1,69 @@
+from mock import patch
+from unittest import TestCase
+
+from tornado.web import HTTPError
+
+from brainiak.stored_query import execution
+
+
+class StoredQueryExecuteTestCase(TestCase):
+
+    def test_get_query_with_valid_params_in_request(self):
+        expected = "SELECT ?s FROM <http://my_graph.com/> {?s a owl:Class}"
+        stored_query = {
+            "sparql_template": "SELECT ?s FROM <%(graph_uri)s> {?s a owl:Class}"
+        }
+
+        class QueryStringParams(object):
+            arguments = {"graph_uri": "http://my_graph.com/"}
+
+        response = execution.get_query(stored_query, QueryStringParams())
+        self.assertEqual(expected, response)
+
+    @patch("brainiak.stored_query.execution._", return_value="msg")
+    def test_get_query_with_missing_param_in_request(self, mocked_i18n_message):
+        stored_query = {
+            "sparql_template": "SELECT ?s FROM <%(graph_uri)s> {?s a owl:Class}"
+        }
+
+        class QueryStringParams(object):
+            arguments = {"a": "an_invalid_param"}
+
+        self.assertRaises(HTTPError,
+                          execution.get_query,
+                          stored_query,
+                          QueryStringParams())
+
+    @patch("brainiak.stored_query.execution.compress_keys_and_values",
+           return_value=[])
+    @patch("brainiak.stored_query.execution.query_sparql")
+    @patch("brainiak.stored_query.execution.get_query",
+           return_value="SELECT ?s FROM <http://my_graph.com/> {?s a owl:Class}")
+    def test_execute_query_with_no_results(self,
+                                           mock_get_query,
+                                           mock_query_sparql,
+                                           mock_compress):
+        query_id = "query_id"
+        stored_query = {
+            "sparql_template": "SELECT ?s FROM <%(graph_uri)s> {?s a owl:Class}"
+        }
+
+        class QueryStringParams(object):
+            arguments = {"graph_uri": "http://my_graph.com/"}
+            triplestore_config = {
+                "app_name": "my_app",
+                "query_id": "id",
+                "url": "url",
+                "query": "query"
+            }
+
+        expected_query = stored_query["sparql_template"] % {
+            "graph_uri": QueryStringParams().arguments["graph_uri"],
+        }
+        expected_response = {
+            "items": [],
+            "warning": "The query returned no results. SPARQL endpoint [{0}]\n  Query: {1}".format("url", expected_query)
+        }
+
+        response = execution.execute_query(query_id, stored_query, QueryStringParams())
+        self.assertEqual(expected_response, response)

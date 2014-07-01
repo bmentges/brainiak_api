e889233ef930b7fd49cf0e5f642ca2e1ea84ff26
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2014-07-01 19:34:11 -0300

    Pep8

--- tests/sparql.py
+++ tests/sparql.py
@@ -61,7 +61,7 @@ ISQL = "isql"
 ISQL_CMD = 'echo "%s" | %s'
 ISQL_UP = "DB.DBA.TTLP_MT_LOCAL_FILE('%(ttl)s', '', '%(graph)s');"
 ISQL_DOWN = "SPARQL CLEAR GRAPH <%(graph)s>;"
-ISQL_DROP =  "SPARQL DROP GRAPH <%(graph)s>;"
+ISQL_DROP = "SPARQL DROP GRAPH <%(graph)s>;"
 
 ISQL_SERVER = "select server_root();"
 ISQL_INFERENCE = "rdfs_rule_set('%(graph_uri)sruleset', '%(graph_uri)s');"

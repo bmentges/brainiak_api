6449c980cb9c999b27edf14fcfe166aba1812432
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2014-07-01 15:31:27 -0300

    Add monkey patch to tornado curl_httpclient, to enable PATCH instance tests.

--- tests/integration/test_patch_instance.py
+++ tests/integration/test_patch_instance.py
@@ -1,11 +1,25 @@
 import json
 
 import requests
+import tornado
 from tornado.httputil import HTTPHeaders
 from tests.sparql import QueryTestCase
-from tests.tornado_cases import TornadoAsyncHTTPTestCase
+from tests.tornado_cases import _curl_setup_request, TornadoAsyncHTTPTestCase
 from tornado.testing import AsyncTestCase, AsyncHTTPTestCase
 from brainiak import server
+
+
+# Util tornado 3.1.0.0, tornado.curl_httpclient ignores payloads (body) of
+# custom methods.
+#
+# We fixed this and proposed a pull request to tornado:
+# https://github.com/tornadoweb/tornado/pull/1090
+#
+# While this issue is not fixed (1/7/2014), we are monkey patching
+# tornado.curl_httpclient's _curl_setup_request.
+
+print("Temporary mockey-patch to tornado/curl_httpclient.py")
+tornado.curl_httpclient._curl_setup_request = _curl_setup_request
 
 
 class PatchInstanceTestCase(TornadoAsyncHTTPTestCase, QueryTestCase):

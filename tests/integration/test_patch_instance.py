c39c5c71eb6607d866feb0c6e742d09d01442c42
rodsenra <rodsenra@gmail.com>
2014-07-02 10:48:55 -0300

    factoring out to isolated python module the tornado monkey patch

--- tests/integration/test_patch_instance.py
+++ tests/integration/test_patch_instance.py
@@ -18,8 +18,6 @@ from brainiak import server
 # While this issue is not fixed (1/7/2014), we are monkey patching
 # tornado.curl_httpclient's _curl_setup_request.
 
-print("Temporary mockey-patch to tornado/curl_httpclient.py")
-tornado.curl_httpclient._curl_setup_request = _curl_setup_request
 
 
 class PatchInstanceTestCase(TornadoAsyncHTTPTestCase, QueryTestCase):

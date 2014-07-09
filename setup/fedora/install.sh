c5ae9f18a43e609e122c155d9b03c9527c1eb2ea
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2014-07-09 20:05:59 -0300

    Improve fedora setup script

--- setup/fedora/install.sh
+++ setup/fedora/install.sh
@@ -1,6 +1,9 @@
+#! /bin/sh
 
 # This need to be improved, not tested yet
 # Compatible with Fedora 20
+
+set -e
 
 # export CODE_DIR=~/code
 # export BIN_DIR=~/bin
@@ -10,58 +13,79 @@
 # SYSTEM DEPENDENCIES #
 #*********************#
 
-sudo yum update
-sudo yum -y install $(cat requirements.yum)
+sudo su -p
+yum -y update
+yum -y install $(cat requirements.yum)
 
 #********************#
 # OTHER DEPENDENCIES #
 #********************#
 
 # Virtuoso
-cd $CODE_DIR; git clone https://github.com/openlink/virtuoso-opensource.git
-cd $CODE_DIR/virtuoso-opensource; git checkout develop/6 #5421d2ea8fd344c1d70d7c99cc77a2051914bdc7
-cd $CODE_DIR/virtuoso-opensource; ./autogen.sh
-cd $CODE_DIR/virtuoso-opensource; ./configure
-cd $CODE_DIR/virtuoso-opensource; make
-sudo mkdir -p /var/lib/virtuoso/db/dumps/
-sudo chmod 777 /var/lib/virtuoso/db/ -R
-echo "export VIRTUOSO_HOME=/var/lib/virtuoso/db/" >> ~/.bashrc
-sudo ln -s $CODE_DIR/virtuoso-opensource/binsrc/tests/isql /usr/bin/isql
-sudo ln -s $CODE_DIR/virtuoso-opensource/binsrc/virtuoso/virtuoso-t /usr/bin/virtuoso-t
-cp $CODE_DIR/brainiak_api/setup/fedora/virtuoso.ini $VIRTUOSO_HOME/virtuoso.ini
-cd $VIRTUOSO_HOME; virtuoso-t &
+
+su $USER
+    cd $CODE_DIR
+    git clone https://github.com/openlink/virtuoso-opensource.git
+    cd virtuoso-opensource
+    git checkout 5421d2ea8fd344c1d70d7c99cc77a2051914bdc7 # develop/6
+    ./autogen.sh
+    ./configure
+    make
+exit
+
+ln -s $CODE_DIR/virtuoso-opensource/binsrc/tests/isql /usr/bin/isql
+ln -s $CODE_DIR/virtuoso-opensource/binsrc/virtuoso/virtuoso-t /usr/bin/virtuoso-t
+
+mkdir -p /var/lib/virtuoso/db/dumps/
+chmod 777 /var/lib/virtuoso/db/ -R
+
+su $USER
+    echo "export VIRTUOSO_HOME=/var/lib/virtuoso/db/" >> ~/.bashrc
+    cp $CODE_DIR/brainiak_api/setup/fedora/virtuoso.ini $VIRTUOSO_HOME/virtuoso.ini
+    cd $VIRTUOSO_HOME
+    virtuoso-t &
+exit
 
 # ElasticSearch 0.90.12
-cd $BIN_DIR; wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.12.tar.gz
-cd $BIN_DIR; tar -xvzf elasticsearch-0.90.12.tar.gz
-$BIN_DIR/elasticsearch-0.90.12/bin/elasticsearch -f &
+su $USER
+    cd $BIN_DIR
+    wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.12.tar.gz
+    tar -xvzf elasticsearch-0.90.12.tar.gz
+    elasticsearch-0.90.12/bin/elasticsearch -f &
 
 # ActiveMQ 5.8.0
-cd $BIN_DIR; wget http://archive.apache.org/dist/activemq/apache-activemq/5.8.0/apache-activemq-5.8.0-bin.tar.gz
-cd $BIN_DIR; tar -xvzf apache-activemq-5.8.0-bin.tar.gz
-cp $CODE_DIR/brainiak_api/setup/fedora/activemq.xml $BIN_DIR/apache-activemq-5.8.0/conf/activemq.xml # enable stomp
-cd $BIN_DIR/apache-activemq-5.8.0; bin/activemq start &
+    cd $BIN_DIR
+    wget http://archive.apache.org/dist/activemq/apache-activemq/5.8.0/apache-activemq-5.8.0-bin.tar.gz
+    tar -xvzf apache-activemq-5.8.0-bin.tar.gz
+    cp $CODE_DIR/brainiak_api/setup/fedora/activemq.xml $BIN_DIR/apache-activemq-5.8.0/conf/activemq.xml # enable stomp
+    cd apache-activemq-5.8.0
+    bin/activemq start &
 
 # Redis
-redis-server &
+    redis-server &
 
 #**********************#
 # LOAD SAMPLE ONTOLOGY #
 #**********************#
 
-cd $CODE_DIR/brainiak_api/resources/ontologies; make create_all
-cd $CODE_DIR/brainiak_api/resources/ontologies; make reset_ontologies
+    cd $CODE_DIR/brainiak_api/resources/ontologies
+    make create_all
+    make reset_ontologies
 
 #*********************#
 # PYTHON DEPENDENCIES #
 #*********************#
 
-source /usr/bin/virtualenvwrapper.sh
-mkvirtualenv brainiak
-cd $CODE_DIR/brainiak_api/; make install
+    source /usr/bin/virtualenvwrapper.sh
+    mkvirtualenv brainiak
+    cd $CODE_DIR/brainiak_api/
+    make install
 
 #********************#
 # RUN BRAINIAK TESTS #
 #********************#
 
-cd $CODE_DIR/brainiak_api/; make unit
+    cd $CODE_DIR/brainiak_api/
+    make unit
+
+exit
